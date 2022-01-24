65f2169c5dcdb5aa648ac8cfd6cce4be58b466bb
Christian <christian@strandkarr.com>
2022-01-24 18:24:10 +0100

    Python - 2015 day 2
    Testing out a possible future parser class.

---
+++ python/2015/02/test_aoc_2015_02.py
@@ -0,0 +1,11 @@
+import aoc_2015_02 as a
+
+
+def test_part_1():
+    data = [[2,  3, 4]]
+    assert a.solve_part_1(data) == 58
+
+
+def test_part_2():
+    data = [[2,  3, 4]]
+    assert a.solve_part_2(data) == 34
