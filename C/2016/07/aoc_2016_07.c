a0e852a87c3af74f4eac03b3235750ba10d42a95
Christian <christian@strandkarr.com>
2022-01-27 22:08:40 +0100

    2016 day 7

--- C/2016/07/aoc_2016_07.c
+++ C/2016/07/aoc_2016_07.c
@@ -63,13 +63,64 @@ int solve_part_1(GArray *data) {
     return count;
 }
 
-char *solve_part_2(GArray *data) {
-    return NULL;
+int solve_part_2(GArray *data) {
+    char *string;
+    int count;
+    GError *err = NULL;
+    GMatchInfo *abaInfo, *hypernetInfo;
+    char *aba_str, *hypernet_str, *supernet_str;
+    char bab_str[4];
+    int i;
+
+
+    GRegex *aba = g_regex_new("(?=((\\w)((?!\\2)\\w)\\2))", 0, 0, &err);
+    GRegex *hypernet = g_regex_new("(\\[\\w+\\])", 0, 0, &err);
+    count = 0;
+
+    for (i = 0; i < (int)data->len; i++) {
+        string = g_array_index(data, char *, i);
+        g_regex_match(hypernet, string, 0, &hypernetInfo);
+
+        hypernet_str = g_strdup("");
+        while (g_match_info_matches(hypernetInfo)) {
+            hypernet_str = g_strjoin("", hypernet_str, g_match_info_fetch(hypernetInfo, 1), NULL);
+            g_match_info_next(hypernetInfo, &err);
+        }
+        supernet_str = g_strjoinv("|", g_regex_split(g_regex_new("\\[\\w+\\]", 0, 0, &err), string, 0));
+
+        g_regex_match(aba, supernet_str, 0, &abaInfo);
+        while (g_match_info_matches(abaInfo)){
+            aba_str = g_match_info_fetch(abaInfo, 1);
+            sprintf(bab_str, "%c%c%c", aba_str[1], aba_str[0], aba_str[1]);
+            if (strstr(hypernet_str, bab_str)) {
+                count++;
+                break;
+            }
+            g_match_info_next(abaInfo, &err);
+        }
+        free(supernet_str);
+        free(hypernet_str);
+    }
+    return count;
 }
 
 
-int main(int argc, char **argv) {
+int solve_all(char *filename, int year, int day) {
     GArray *data;
+
+    data = get_input(filename, year, day);
+
+    if (data) {
+        TIMER(1, solve_part_1(data), INT, 1);
+        TIMER(2, solve_part_2(data), INT, 1);
+
+        g_array_free(data, TRUE);
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
     gchar *filename;
 
     if (argc > 1) {
@@ -78,13 +129,7 @@ int main(int argc, char **argv) {
         filename = g_strdup("input.txt");
     }
 
-    data = get_input(filename, 2016, 7);
+    TIMER(0, solve_all(filename, 2016, 7), INT, 0);
 
-    if (data) {
-        TIMER_INT(1, solve_part_1(data));
-        TIMER_STR(2, solve_part_2(data));
-
-        g_array_free(data, TRUE);
-    }
-
+    return 0;
 }
