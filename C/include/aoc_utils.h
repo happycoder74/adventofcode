124a9ad1ce784deb684b5f881639835028acf1dc
Christian <christian@strandkarr.com>
2022-01-27 11:52:47 +0100

    Updated and improved TIMER macro

--- C/include/aoc_utils.h
+++ C/include/aoc_utils.h
@@ -3,23 +3,35 @@
 
 #include <glib.h>
 
-#define TIMER_INT(part, func) \
+#define TIMER(part, func, TYPE, show_res) TIMER_##TYPE(part, func, show_res)
+
+#define TIMER_INT(part, func, show_res) \
     do { \
         GTimer *timer = g_timer_new(); \
         int result = func; \
         g_timer_stop(timer); \
         gdouble elapsed = g_timer_elapsed(timer, NULL); \
-        printf("Part %d answer: %d\t\t( %.3lf %s)\n", part, result, elapsed > 0.1 ? elapsed : elapsed*1000, elapsed > 0.1? "s":"ms"); \
+		elapsed = elapsed > 0.1 ? elapsed : elapsed*1000; \
+		if (show_res) { \
+			printf("Part %d answer: %d\t\t( %.3lf %s)\n", part, result, elapsed, elapsed > 0.1? "s":"ms"); \
+		} else { \
+			printf("Time elapsed:\t\t\t( %.3lf %s)\n", elapsed, elapsed > 0.1? "s":"ms"); \
+		} \
         g_timer_destroy(timer); \
     } while (0)
 
-#define TIMER_STR(part, func) \
+#define TIMER_STR(part, func, show_res) \
     do { \
         GTimer *timer = g_timer_new(); \
         gchar *result = func; \
         g_timer_stop(timer); \
         gdouble elapsed = g_timer_elapsed(timer, NULL); \
-        printf("Part %d answer: %s\t\t( %.3lf %s)\n", part, result, elapsed > 0.1 ? elapsed : elapsed*1000, elapsed > 0.1? "s":"ms"); \
+		elapsed = elapsed > 0.1 ? elapsed : elapsed*1000; \
+		if (show_res) { \
+			printf("Part %d answer: %s\t\t( %.3lf %s)\n", part, result, elapsed, elapsed > 0.1? "s":"ms"); \
+		} else { \
+			printf("Time elapsed:\t\t\t( %.3lf %s)\n", elapsed, elapsed > 0.1? "s":"ms"); \
+		} \
         g_timer_destroy(timer); \
     } while (0)
 
