1882c2e5d83e78abbb9295bfd1c6fe17dca5a47a
Christian <christian@strandkarr.com>
2022-01-28 14:36:59 +0100

    Updated test case for 2016 day 7

--- python/2016/07/test_aoc_2016_07.py
+++ python/2016/07/test_aoc_2016_07.py
@@ -2,12 +2,12 @@ import unittest
 import aoc_2016_07 as a
 
 
-class Test_2016_7(unittest.TestCase):
+class Test_2016_07(unittest.TestCase):
+    def setUp(self):
+        self.data = a.clean_input(a.get_input("test_input.txt", 2016, 7))
 
     def test_part_1(self):
-        data = a.get_input("test_input.txt", 2016, 7)
-        self.assertEqual(2, a.solve_part_1(data))
+        self.assertEqual(2, a.solve_part_1(self.data))
 
     def test_part_2(self):
-        data = a.get_input("test_input.txt", 2016, 7)
-        self.assertEqual(3, a.solve_part_2(data))
+        self.assertEqual(3, a.solve_part_2(self.data))
