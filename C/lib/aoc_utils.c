f9f55c13a11a089636899bf39c56254c57e18fbf
Christian <christian@strandkarr.com>
2022-01-28 09:26:32 +0100

    Comment formatting only

--- C/lib/aoc_utils.c
+++ C/lib/aoc_utils.c
@@ -6,25 +6,26 @@
 #include <assert.h>
 #include <ctype.h>
 
-// Note: This function returns a pointer to a substring of the original string.
-// If the given string was allocated dynamically, the caller must not overwrite
-// that pointer with the returned value, since the original pointer must be
-// deallocated using the same allocator with which it was allocated.  The return
-// value must NOT be deallocated using free() etc.
+/* Note: This function returns a pointer to a substring of the original string.
+If the given string was allocated dynamically, the caller must not overwrite
+that pointer with the returned value, since the original pointer must be
+deallocated using the same allocator with which it was allocated.  The return
+value must NOT be deallocated using free() etc. */
+
 char *str_trim(char *str) {
   char *end;
 
-  // Trim leading space
+  /* Trim leading space */
   while(isspace((unsigned char)*str)) str++;
 
-  if(*str == 0)  // All spaces?
+  if(*str == 0)  /* All spaces? */
     return str;
 
-  // Trim trailing space
+  /* Trim trailing space */
   end = str + strlen(str) - 1;
   while(end > str && isspace((unsigned char)*end)) end--;
 
-  // Write new null terminator character
+  /* Write new null terminator character */
   end[1] = '\0';
 
   return str;
