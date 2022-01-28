adb92fea37637497078a516a4f76d927a618a099
Christian <christian@strandkarr.com>
2022-01-28 10:29:48 +0100

    Python 2015 day 6 test file updated

--- python/2015/06/test_aoc_2015_06.py
+++ python/2015/06/test_aoc_2015_06.py
@@ -3,10 +3,11 @@ import aoc_2015_06 as a
 
 
 class Test_2015_06(unittest.TestCase):
+    def setUp(self):
+        self.data = a.clean_input(a.get_input("test_input.txt", 2015, 6))
 
     def test_part_1(self):
-        data = a.clean_input(a.get_input("test_input.txt", 2015, 6))
-        self.assertEqual(1000, a.solve_part_1(data))
+        self.assertEqual(1000, a.solve_part_1(self.data))
 
     def test_part_2(self):
         data = [('toggle', (0, 1000), (0, 1000))]
