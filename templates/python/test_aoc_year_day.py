3ecb49909452eaa2f4429991f230e6c2c46eb564
Christian <christian@strandkarr.com>
2022-01-28 11:24:05 +0100

    Fixed another bug in test template for python

--- templates/python/test_aoc_year_day.py
+++ templates/python/test_aoc_year_day.py
@@ -4,7 +4,9 @@ import aoc_<YEAR>_<DAY> as a
 
 class Test_<YEAR>_<DAY>(unittest.TestCase):
     def setUp(self):
-        self.data = a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
+        self.data = a.clean_input(
+            a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
+        )
 
     def test_part_1(self):
         self.assertEqual(None, a.solve_part_1(self.data))
