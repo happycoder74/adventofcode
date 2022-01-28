541ea9b007bf5e025aa2d1d41aa255fc49be5c17
Christian <christian@strandkarr.com>
2022-01-28 10:31:09 +0100

    Updated test template for python

--- templates/python/test_aoc_year_day.py
+++ templates/python/test_aoc_year_day.py
@@ -3,11 +3,11 @@ import aoc_<YEAR>_<DAY> as a
 
 
 class Test_year_day(unittest.TestCase):
+    def setUp(self):
+        self.data = a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
 
     def test_part_1(self):
-        data = a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
-        self.assertEqual(None, a.solve_part_1(data))
+        self.assertEqual(None, a.solve_part_1(self.data))
 
     def test_part_2(self):
-        data = a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
-        self.assertEqual(None, a.solve_part_2(data))
+        self.assertEqual(None, a.solve_part_2(self.data))
