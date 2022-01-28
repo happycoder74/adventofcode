a1cd18235cba4e9495fdaf12881b414879b112c4
Christian <christian@strandkarr.com>
2022-01-28 14:37:06 +0100

    Updated test case for 2016 day 8

--- python/2016/08/test_aoc_2016_08.py
+++ python/2016/08/test_aoc_2016_08.py
@@ -3,11 +3,18 @@ import aoc_2016_08 as a
 
 
 class Test_2016_08(unittest.TestCase):
+    def setUp(self):
+        self.data = a.clean_input(a.get_input("test_input.txt", 2016, 8))
 
     def test_part_1(self):
-        data = a.clean_input(a.get_input("test_input.txt", 2016, 08))
-        self.assertEqual(None, a.solve_part_1(data))
+        self.assertEqual(6, a.solve_part_1(self.data))
 
-    def test_part_2(self):
-        data = a.clean_input(a.get_input("test_input.txt", 2016, 08))
-        self.assertEqual(None, a.solve_part_2(data))
+    def test_part_1_1(self):
+        self.assertEqual(9, a.solve_part_1(a.clean_input(['rect 3x3'])))
+
+    def test_part_1_2(self):
+        data = [
+            'rect 9x6',
+            'rotate row y=1 by 1'
+        ]
+        self.assertEqual(54, a.solve_part_1(a.clean_input(data)))
