0c2f05a9cb83045109466a25a7c12b44acde850c
Christian <christian@strandkarr.com>
2022-01-28 10:40:21 +0100

    2015 day 2 Updated test file

--- python/2015/02/test_aoc_2015_02.py
+++ python/2015/02/test_aoc_2015_02.py
@@ -1,11 +1,15 @@
+import unittest
 import aoc_2015_02 as a
 
 
-def test_part_1():
-    data = [[2,  3, 4]]
-    assert a.solve_part_1(data) == 58
+class Test_2015_02(unittest.TestCase):
+    def setUp(self):
+        pass
 
+    def test_part_1(self):
+        data = [[2,  3, 4]]
+        self.assertEqual(58, a.solve_part_1(data))
 
-def test_part_2():
-    data = [[2,  3, 4]]
-    assert a.solve_part_2(data) == 34
+    def test_part_2(self):
+        data = [[2,  3, 4]]
+        self.assertEqual(34, a.solve_part_2(data))
