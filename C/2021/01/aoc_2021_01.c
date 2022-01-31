32f35622eee13e8aeb1c42838e0d88ff9ebb1343
Christian <christian@strandkarr.com>
2022-01-31 18:35:35 +0100

    C 2021 day 1 Another structural update

--- C/2021/01/aoc_2021_01.c
+++ C/2021/01/aoc_2021_01.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include "aoc_utils.h"
 
+GArray *clean_data(GArray *data) {
+    return data;
+}
+
 int solve_part_1(GArray *data) {
-    gint i;
+    guint i;
     gint count = 0;
     char *val1, *val2;
 
@@ -18,7 +22,7 @@ int solve_part_1(GArray *data) {
 }
 
 int solve_part_2(GArray *data) {
-    gint i;
+    guint i;
     gint count = 0;
     char *val1, *val2;
 
@@ -33,18 +37,32 @@ int solve_part_2(GArray *data) {
 }
 
 
-int main(int argc, char **argv) {
+int solve_all(char *filename, int year, int day) {
     GArray *data;
+
+    data = clean_data(get_input(filename, year, day));
+
+    if (data) {
+        TIMER(1, solve_part_1(data), INT, 1);
+        TIMER(2, solve_part_2(data), INT, 1);
+
+        g_array_free(data, TRUE);
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
     char *filename;
 
     if (argc > 1) {
-        filename = argv[1];
+        filename = g_strdup(argv[1]);
     } else {
-        filename = "input.txt";
+        filename = g_strdup("input.txt");
     }
 
-    data = get_input(filename, 2021, 1);
+    TIMER(0, solve_all(filename, 2021, 1), INT, 0);
 
-    TIMER_INT(1, solve_part_1(data));
-    TIMER_INT(2, solve_part_2(data));
+    g_free(filename);
+
 }
