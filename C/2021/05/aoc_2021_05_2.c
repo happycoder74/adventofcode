0b3a0dcf7b3a7cfb3b3c336511c226ffbb999790
Christian <christian@strandkarr.com>
2022-02-15 16:46:15 +0100

    Alternate versions for day 5
    aoc_2021_05_2: Uses a statically allocated grid array. Fastest of the three
    aoc_2021_05_3: Uses a dynamically allocated grid array. Second fastest of the three
                   but almost twice the time compared to _2.
    
    Both alternates almost an order of magnitude faster than the GHashTable solution.

---
+++ C/2021/05/aoc_2021_05_2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+#include "aoc_utils.h"
+
+#define GRIDSIZE 1000
+
+
+GArray *clean_input(GArray *data) {
+    gchar *dataline;
+    GArray *return_data;
+    Line line;
+
+    return_data = g_array_new(TRUE, FALSE, sizeof(Line));
+    for (guint i = 0; i < data->len; i++) {
+        dataline = g_array_index(data, gchar *, i);
+        sscanf(dataline, "%d,%d -> %d,%d", &line.p0.x, &line.p0.y, &line.p1.x, &line.p1.y);
+        if (is_horisontal(line)) {
+            line.stepx = line.p0.x < line.p1.x ? 1 : -1;
+            line.stepy = 0;
+        } else if (is_vertical(line)) {
+            line.stepx = 0;
+            line.stepy = line.p0.y < line.p1.y ? 1 : -1;
+        } else {
+            line.stepx = line.p0.x < line.p1.x ? 1 : -1;
+            line.stepy = line.p0.y < line.p1.y ? 1 : -1;
+        }
+        g_array_append_val(return_data, line);
+    }
+
+    g_array_unref(data);
+    return return_data;
+}
+
+gboolean is_diagonal (Line line) {
+    return (!is_horisontal(line)) && (!is_vertical(line));
+}
+
+void mark_points(gint *grid, Line line, gboolean diagonal) {
+    Point point;
+
+    if (!diagonal && is_diagonal(line)) {
+        return;
+    }
+
+    for (point = line.p0;
+            (point.x != (line.p1.x + line.stepx)) || (point.y != (line.p1.y + line.stepy));
+            point.x += line.stepx, point.y += line.stepy) {
+        *((grid + point.x*GRIDSIZE) + point.y) += 1;
+    }
+    return;
+}
+
+
+int solve_problem(GArray *data, gboolean diagonal) {
+    Line line;
+    gint row, col;
+    gint count;
+    gint grid[GRIDSIZE][GRIDSIZE] = {0};
+
+    for (guint i = 0; i < data->len; i++) {
+        line = g_array_index(data, Line, i);
+        mark_points((int *)grid, line, diagonal);
+    }
+
+    count = 0;
+    for (row = 0; row < GRIDSIZE; row++) {
+        for (col = 0; col < GRIDSIZE; col++) {
+            if (grid[col][row] > 1)
+                count++;
+        }
+    }
+
+    return count;
+}
+
+int solve_part_1(GArray *data) {
+    return solve_problem(data, FALSE);
+}
+
+int solve_part_2(GArray *data) {
+    return solve_problem(data, TRUE);
+}
+
+int solve_all(gchar *filename, int year, int day) {
+    GArray *data;
+
+    data = clean_input(get_input(filename, year, day));
+
+    if (data) {
+        TIMER(1, solve_part_1(data), INT, 1);
+        TIMER(2, solve_part_2(data), INT, 1);
+
+        g_array_free(data, TRUE);
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    gchar *filename;
+
+    if (argc > 1) {
+        filename = g_strdup(argv[1]);
+    } else {
+        filename = g_strdup("input.txt");
+    }
+
+    TIMER(0, solve_all(filename, 2021, 5), INT, 0);
+    g_free(filename);
+}
+
