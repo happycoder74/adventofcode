3409c114b7d4c51f7d2f2e75aafe0a40ffd9a688
Christian Lundh <christianlundh1974@gmail.com>
2022-11-26 12:18:05 +0100

    Removed unused declaration and definition of timer_func_str and timer_funt_int

--- C/include/aoc_timer.h
+++ C/include/aoc_timer.h
@@ -5,8 +5,6 @@
 #include "aoc_types.h"
 
 void timer_func(int, gpointer (func)(AocData_t *), AocData_t *, gboolean);
-void timer_func_str(int, gchar *(func)(GArray *), GArray *, int);
-void timer_func_int(int, int(func)(GArray *), GArray *, int);
 
 #define TIMER(part, func, TYPE, show_res) TIMER_##TYPE(part, func, show_res)
 
