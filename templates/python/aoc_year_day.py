2a2d223958efe24ca48d416d742ccd5174f5f0f3
Christian Lundh <christianlundh1974@gmail.com>
2022-12-01 09:22:40 +0100

    New class based Puzzle structure

--- templates/python/aoc_year_day.py
+++ templates/python/aoc_year_day.py
@@ -1,35 +1,25 @@
 import sys
-from common import timer, get_input
+from common import timer, Puzzle
 
 
-def clean_input(data):
-    return data
+class Day<DAY>(Puzzle, year=<YEAR>, day=<DAY>):
+    @staticmethod
+    def clean_input(data):
+        return data
 
+    @timer(part=1)
+    def solve_part_1(self):
+        """Solution for part 1"""
+        return None
 
-@timer(part=1)
-def solve_part_1(data):
-    """Solution for part 1"""
-    return None
-
-
-@timer(part=2)
-def solve_part_2(data):
-    """Solution for part 2"""
-    return None
-
-
-@timer(part='main', title='Total elapsed', show_return=False)
-def main(filename):
-    data = clean_input(get_input(filename, <YEAR>, <DAY>))
-
-    part1 = solve_part_1(data)
-    part2 = solve_part_2(data)
-
-    return part1, part2
+    @timer(part=2)
+    def solve_part_2(self):
+        """Solution for part 2"""
+        return None
 
 
 if __name__ == "__main__":
     filename = "input.txt"
     if len(sys.argv) > 1:
         filename = sys.argv[1]
-    main(filename)
+    Day<DAY>(filename=filename).solve_all()
