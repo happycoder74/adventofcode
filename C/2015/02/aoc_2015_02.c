692e59e31eba8cc7614d78a52d4fb16979c7a897
Christian <christian@strandkarr.com>
2022-12-01 12:11:15 +0100

    Implemented new timer function and aoc data structure

--- C/2015/02/aoc_2015_02.c
+++ C/2015/02/aoc_2015_02.c
@@ -2,53 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <glib.h>
+#include "aoc_types.h"
 #include "aoc_utils.h"
 
 
-gchar *solve_part_1(GArray *data) {
+gpointer solve_part_1(AocData_t *data) {
     guint i;
     gchar *row;
     gint l, w, h;
     gint area = 0;
 
-    for (i = 0; i < data->len; i++) {
-        row = g_array_index(data, char *, i);
+    for (i = 0; i < data->data->len; i++) {
+        row = g_array_index(data->data, char *, i);
         sscanf(row, "%dx%dx%d", &l, &w, &h);
         area += 2*l*w + 2*w*h + 2*h*l + MIN(MIN(l*w, w*h), h*l);
     }
     return g_strdup_printf("%d", area);
 }
 
-gchar *solve_part_2(GArray *data) {
+gpointer solve_part_2(AocData_t *data) {
     guint i;
     gchar *row;
     gint l, w, h;
     gint ribbon = 0;
 
-    for (i = 0; i < data->len; i++) {
-        row = g_array_index(data, char *, i);
+    for (i = 0; i < data->data->len; i++) {
+        row = g_array_index(data->data, char *, i);
         sscanf(row, "%dx%dx%d", &l, &w, &h);
         ribbon += MIN(MIN(2*w + 2*l, 2*w + 2*h), 2*h + 2*l) + l*w*h;
     }
     return g_strdup_printf("%d", ribbon);
 }
 
-int solve_all(gchar *filename, int year, int day) {
-    GArray *data;
+gpointer solve_all(AocData_t *data) {
+    data->data = get_input(data->filename, data->year, data->day);
 
-    data = get_input(filename, year, day);
-
-    if (data) {
-        timer_func_str(1, solve_part_1, data, 1);
-        timer_func_str(2, solve_part_2, data, 1);
-
-        g_array_free(data, TRUE);
+    if (data->data) {
+        timer_func(1, solve_part_1, data, 1);
+        timer_func(2, solve_part_2, data, 1);
     }
 
-    return 0;
+    return NULL;
 }
 
 int main(int argc, char **argv) {
+    AocData_t *data;
     gchar *filename;
 
     if (argc > 1) {
@@ -57,7 +55,11 @@ int main(int argc, char **argv) {
         filename = g_strdup("input.txt");
     }
 
-    TIMER(0, solve_all(filename, 2015, 2), INT, 0);
-
+    data = aoc_data_new(filename, 2015, 2);
     g_free(filename);
+    timer_func(0, solve_all, data, 0);
+
+    aoc_data_free(data);
+
+    return 0;
 }
