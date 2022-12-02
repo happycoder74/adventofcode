72bac077bfed1d8042587000d8b1258c303f661d
Christian Lundh <christianlundh1974@gmail.com>
2022-12-02 16:29:18 +0100

    Added runner application
    Usage python runner.py <year> <day>
    
    If option <year> is specified, runs all available solutions for <year>
    If option <day> is specified (<year> must be speced as well) only run solution for <year>/<day>
    
    If no options are given, all available solutions for all days will be executed.

---
+++ python/runner.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+from importlib.machinery import SourceFileLoader
+
+
+class AocRunner(object):
+    def __init__(self, year=None, day=None, filename=None):
+        self.year = year
+        self.day = day
+        if filename is not None:
+            self.filename = filename
+        else:
+            self.filename = "input.txt"
+
+    def run(self):
+        class_list = self.get_classes()
+        for year, cls in class_list:
+            print(f"Year {year}: {cls.__name__}")
+            print("=================================")
+            try:
+                cls(filename=self.filename).solve_all()
+            except ModuleNotFoundError:
+                print("Unable to import submodule")
+            print("=================================")
+            print("")
+
+    def get_classes(self):
+        class_list = list()
+        if self.day is not None:
+            days = [int(self.day)]
+        else:
+            days = range(1, 26)
+
+        if self.year is not None:
+            year = [int(self.year)]
+        else:
+            year = range(2015, 2023)
+
+        for year in year:
+            for day in days:
+                module = f"aoc_{year}_{day:02d}"
+                sourcefile = os.path.join(f"{year}", f"{day:02d}", f"{module}.py")
+                if os.path.exists(sourcefile):
+                    try:
+                        puzzle = SourceFileLoader(module, sourcefile).load_module()
+                        try:
+                            cls = getattr(puzzle, f"Day{day}")
+                            class_list.append((year, cls))
+                        except AttributeError:
+                            print(f"No solution for day {day} for {year}")
+                    except ModuleNotFoundError:
+                        print(f"Unable to import submodule for {year} - {day}")
+        self.year = year
+        return class_list
+
+
+if __name__ == "__main__":
+    arg_year = None
+    arg_day = None
+    if len(sys.argv) == 2:
+        arg_year = sys.argv[1]
+        arg_day = None
+    if len(sys.argv) == 3:
+        arg_year = sys.argv[1]
+        arg_day = sys.argv[2]
+    runner = AocRunner(year=arg_year, day=arg_day)
+    runner.run()
