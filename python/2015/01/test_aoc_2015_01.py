1a91cb07a87e051135638ddd51718c1d783785fb
Christian Lundh <christianlundh1974@gmail.com>
2022-12-04 00:49:48 +0100

    Updated 2015 / 01 test file

--- python/2015/01/test_aoc_2015_01.py
+++ python/2015/01/test_aoc_2015_01.py
@@ -4,10 +4,10 @@ from aoc_2015_01 import Day1
 
 class Test_2015_01(unittest.TestCase):
     def setUp(self):
-        self.data = Day1(filename="input.txt")
+        self.day = Day1(filename="input.txt")
 
     def test_part_1(self):
-        self.data.data = ')())())'
+        self.day.data = ')())())'
         self.assertEqual(-3, self.data.solve_part_1())
 
     def test_part_2(self):
