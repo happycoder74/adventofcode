9cf9fa6df2dde25f5ea0b3e87f605779064268c0
Christian Lundh <christianlundh1974@gmail.com>
2022-12-04 00:44:14 +0100

    Class based method implemented

--- python/2015/01/test_aoc_2015_01.py
+++ python/2015/01/test_aoc_2015_01.py
@@ -1,13 +1,15 @@
 import unittest
-import aoc_2015_01 as a
+from aoc_2015_01 import Day1
 
 
 class Test_2015_01(unittest.TestCase):
+    def setUp(self):
+        self.data = Day1(filename="input.txt")
 
     def test_part_1(self):
-        data = ')())())'
-        self.assertEqual(-3, a.solve_part_1(data))
+        self.data.data = ')())())'
+        self.assertEqual(-3, self.data.solve_part_1())
 
     def test_part_2(self):
-        data = '()())'
-        self.assertEqual(5, a.solve_part_2(data))
+        self.data.data = '()())'
+        self.assertEqual(5, self.data.solve_part_2())
