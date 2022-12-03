9d47c0442c3c9d4a8696de7e1da0d527d520e99f
Christian Lundh <christianlundh1974@gmail.com>
2022-12-04 00:49:18 +0100

    Small fix but not fully functional

--- templates/python/test_aoc_year_day.py
+++ templates/python/test_aoc_year_day.py
@@ -1,15 +1,15 @@
 import unittest
-from aoc_<YEAR>_<DAY> import Day<DAY> as a
+from aoc_<YEAR>_<DAY> import Day<DAY>
 
 
 class Test_<YEAR>_<DAY>(unittest.TestCase):
     def setUp(self):
-        self.data = a.clean_input(
-            a.get_input("test_input.txt", <YEAR>, <CORR_DAY>)
-        )
+        self.day = Day<DAY>(filename="input.txt")
 
     def test_part_1(self):
-        self.assertEqual(None, a.solve_part_1(self.data))
+        self.day.data = ""
+        self.assertEqual(None, self.day.solve_part_1())
 
     def test_part_2(self):
-        self.assertEqual(None, a.solve_part_2(self.data))
+        self.day.data = ""
+        self.assertEqual(None, self.day.solve_part_2())
