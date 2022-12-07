0cfa313f7231792ddd085321493c968e5d75bab0
Christian Lundh <christianlundh1974@gmail.com>
2022-12-07 22:09:34 +0100

    Smarter (much smarter) version of 2020 day 5.
    I felt it all the time, there must be a better way. After looking around
    I realised what I missed. We were simply given binary numbers. In another form

--- python/2020/05/aoc_2020_05.py
+++ python/2020/05/aoc_2020_05.py
@@ -5,58 +5,24 @@ from common import timer, Puzzle
 class Day5(Puzzle, year=2020, day=5):
     @staticmethod
     def clean_input(data):
-        return data
+        return sorted([Day5.find_id(row) for row in data])
 
-    def find_row(self, data):
-        first_row = 0
-        last_row = 127
-        for letter in data[:-1]:
-            if letter == "F":
-                last_row = (last_row - first_row + 1) / 2 + first_row - 1
-            else:
-                first_row = (last_row - first_row + 1) / 2 + first_row
-        if (data[-1] == "F"):
-            return first_row
-        else:
-            return last_row
-
-    def find_seat(self, data):
-        first = 0
-        last = 7
-        for letter in data[:-1]:
-            if letter == "L":
-                last = (last - first + 1) / 2 + first - 1
-            else:
-                first = (last - first + 1) / 2 + first
-        if (data[-1] == "L"):
-            return first
-        else:
-            return last
+    @staticmethod
+    def find_id(data: str):
+        seat_id = data.translate(str.maketrans('FBLR', '0101'))
+        return int(seat_id, 2)
 
     @timer(part=1)
     def solve_part_1(self):
         """Solution for part 1"""
-        max_id = 0
-        for row in self.data:
-            seat_id = (self.find_row(row[0:7]) * 8 +
-                       self.find_seat(row[7:]))
-            if seat_id >= max_id:
-                max_id = seat_id
-        return max_id
+        return self.data[-1]
 
     @timer(part=2)
     def solve_part_2(self):
         """Solution for part 2"""
-        seat_id_list = []
-        for row in self.data:
-            seat_id = (self.find_row(row[0:7]) * 8 +
-                       self.find_seat(row[7:]))
-            seat_id_list.append(int(seat_id))
-        seat_id_list.sort()
-        for index, seat in enumerate(seat_id_list[:-1]):
-            if seat_id_list[index + 1] != seat + 1:
-                return seat + 1
-        return None
+        seats = self.data
+        all_seats = set(range(seats[0], seats[-1] + 1))
+        return all_seats.difference(seats).pop()
 
 
 if __name__ == "__main__":
