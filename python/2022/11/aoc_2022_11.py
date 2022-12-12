f7a12269642f69d3aac9546f1e792d0070a80dc8
Christian Lundh <christianlundh1974@gmail.com>
2022-12-12 22:42:39 +0100

    Python 2022 day 11 with working solution also for part 2. So easy once you figure it out

--- python/2022/11/aoc_2022_11.py
+++ python/2022/11/aoc_2022_11.py
@@ -1,9 +1,12 @@
 import sys
+from functools import reduce
+from operator import mul
 from common import timer, Puzzle
 
 
 class Monkey(object):
-    def __init__(self, group: list = None, id=None):
+    def __init__(self, group: list = None, id=None, reliever: int = 3):
+        super().__init__()
         self.id = id
         self.items = [int(item.strip())
                       for item in group[1].split(":")[1].split(",")]
@@ -12,6 +15,8 @@ class Monkey(object):
         self.receiver_true = int(group[4][-1])
         self.receiver_false = int(group[5][-1])
         self.inspected_items = 0
+        self.reliever = reliever
+        self.lcm = 1
         # self.report()
 
     def perform_operation(self, item):
@@ -44,6 +49,7 @@ class Monkey(object):
         # throw to monkey based on criteria
         result = self.throw(divisor)
         self.inspected_items += 1
+        result %= self.lcm
         if result % self.divisor == 0:
             return (self.receiver_true, result)
         else:
@@ -57,34 +63,39 @@ class Monkey(object):
 
 
 class Day11(Puzzle, year=2022, day=11):
-    def __init__(self, filename=None, data=None):
+    def __init__(self, filename=None, data=None) -> None:
         super().__init__(filename=filename, data=data)
+        self.init_monkeys(reliever=3)
+
+    def init_monkeys(self, reliever) -> None:
+        group: list
+        self.monkeys: list
+
         self.monkeys = list()
 
         for group in self.data:
-            monkey = Monkey(group=group, id=len(self.monkeys))
+            monkey: Monkey = Monkey(group=group, id=len(self.monkeys), reliever=reliever)
             self.monkeys.append(monkey)
+        lcm = reduce(mul, [monkey.divisor for monkey in self.monkeys])
+        for monkey in self.monkeys:
+            monkey.lcm = lcm
 
     @staticmethod
-    def clean_input(data):
+    def clean_input(data: list):
         return Puzzle.parse_input_groups(data)
 
-    def round(self, divisor):
+    def round(self, divisor: int):
         monkey: Monkey
         for monkey in self.monkeys:
             while monkey.items:
                 receiver, item = monkey.inspect(divisor)
                 self.monkeys[receiver].receive(item)
 
-    def solve(self, rounds=20, divisor=3):
+    def solve(self, rounds: int = 20, divisor: int = 3):
+        round: int
+        inspected: list
         for round in range(rounds):
             self.round(divisor)
-            if round % 100 == 0:
-                inspected = sorted(
-                    [monkey.inspected_items for monkey in self.monkeys]
-                )
-                monkey_list = [len(monkey.items) for monkey in self.monkeys]
-                print(f"{round=}: {inspected}, {monkey_list=}")
         inspected = sorted([monkey.inspected_items for monkey in self.monkeys])
 
         return inspected[-1]*inspected[-2]
@@ -97,7 +108,11 @@ class Day11(Puzzle, year=2022, day=11):
     @timer(part=2)
     def solve_part_2(self):
         """Solution for part 2"""
-        return self.solve(rounds=10000, divisor=1)
+        self.init_monkeys(reliever=1)
+        try:
+            return self.solve(rounds=10000, divisor=1)
+        except KeyboardInterrupt:
+            print(max([max(monkey.items) for monkey in self.monkeys]))
 
 
 if __name__ == "__main__":
