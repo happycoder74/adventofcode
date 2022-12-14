aed39f90bf943bb23a50a90e296aabbf4b231e3d
Christian Lundh <christianlundh1974@gmail.com>
2022-12-14 21:47:06 +0100

    Converted to new Puzzle structure. Imported from old repo

---
+++ python/2020/06/test_aoc_2020_06.py
@@ -0,0 +1,13 @@
+import unittest
+from aoc_2020_06 import Day06
+
+
+class Test_2020_06(unittest.TestCase):
+    def setUp(self):
+        self.day = Day06(filename="test_input.txt")
+
+    def test_part_1(self):
+        self.assertEqual(11, self.day.solve_part_1())
+
+    def test_part_2(self):
+        self.assertEqual(6, self.day.solve_part_2())
