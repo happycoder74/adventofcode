8feb92579cfb26e6b3c8fd86785662e8220357eb
Christian Lundh <christianlundh1974@gmail.com>
2022-12-27 10:57:53 +0100

    Using sets to track and combine the ranges

--- python/y2022/d15/aoc_2022_15.py
+++ python/y2022/d15/aoc_2022_15.py
@@ -2,6 +2,24 @@ import re
 from common import timer, Puzzle
 
 
+def combine_ranges(ranges):
+    print(ranges)
+    updated_ranges = list()
+    non_matching_ranges = list()
+    p_start, p_end = ranges[0]
+    for r_start, r_end in ranges[1:]:
+        if r_start <= p_end and p_start <= r_end:
+            updated_ranges.append((min(r_start, p_start), max(r_end, p_end)))
+        else:
+            non_matching_ranges.append((r_start, r_end))
+    if updated_ranges:
+        return combine_ranges(updated_ranges + non_matching_ranges)
+    elif non_matching_ranges:
+        return [(p_start, p_end)] + combine_ranges(non_matching_ranges)
+    else:
+        return [(p_start, p_end)]
+
+
 class Day15(Puzzle, year=2022, day=15):
     def __init__(self, filename=None, data=None):
         super().__init__(filename=filename, data=data)
@@ -12,26 +30,35 @@ class Day15(Puzzle, year=2022, day=15):
     def clean_input(data):
         sensors = {}
         for row in data:
-            x, y, xx, yy = list(map(int, re.findall(pattern=r"\w*=(-{0,1}\d+)", string=row)))
-            sensors[(x, y)] = {'beacon': (xx, yy), 'distance': (abs(x - xx) + abs(y - yy))}
+            x, y, xx, yy = list(
+                map(int, re.findall(pattern=r"\w*=(-{0,1}\d+)", string=row))
+            )
+            sensors[(x, y)] = {
+                "beacon": (xx, yy),
+                "distance": (abs(x - xx) + abs(y - yy)),
+            }
 
         return sensors
 
     def setup_grid(self):
         for pos, beacon in self.data.items():
-            self.grid[pos] = 'S'
-            self.grid[beacon['beacon']] = 'B'
+            self.grid[pos] = "S"
+            self.grid[beacon["beacon"]] = "B"
 
     def print_grid(self):
-        limits_x = (min(p[0] for p in self.grid.keys()),
-                    max(p[0] for p in self.grid.keys()))
-        limits_y = (min(p[1] for p in self.grid.keys()),
-                    max(p[1] for p in self.grid.keys()))
+        limits_x = (
+            min(p[0] for p in self.grid.keys()),
+            max(p[0] for p in self.grid.keys()),
+        )
+        limits_y = (
+            min(p[1] for p in self.grid.keys()),
+            max(p[1] for p in self.grid.keys()),
+        )
         for row in range(limits_y[0], limits_y[1] + 1):
             print(f"{row:4d}", end="")
             for col in range(limits_x[0], limits_x[1] + 1):
-                if self.grid.get((col, row), ' ') in ['S', 'B', '#']:
-                    print(self.grid.get((col, row), ' '), end="")
+                if self.grid.get((col, row), " ") in ["S", "B", "#"]:
+                    print(self.grid.get((col, row), " "), end="")
                 else:
                     print(".", end="")
             print()
@@ -39,35 +66,30 @@ class Day15(Puzzle, year=2022, day=15):
     @timer(part=1)
     def solve_part_1(self, row=2000000):
         """Solution for part 1"""
-        blocked_pos_list = set()
-        for (x, y), beacon in self.data.items():
-            if (y - beacon['distance']) <= row <= (y + beacon['distance']):
-                yy = list(range(y - beacon['distance'], y + beacon['distance'] + 1))
-                xx = beacon['distance'] - abs(yy[yy.index(row)] - y)
-                for dx in range(-xx, xx + 1):
-                    blocked_pos = (x + dx, row)
-                    if self.grid.get(blocked_pos) not in ['S', 'B']:
-                        blocked_pos_list.add(blocked_pos)
-                        self.grid[blocked_pos] = "#"
-
         if self.filename == "test_input.txt":
-            self.print_grid()
+            row = 10
 
-        return len(blocked_pos_list)
+        ranges = set()
+
+        for (x, y), beacon in self.data.items():
+            distance = abs(y - row)
+            if distance <= beacon["distance"]:
+                ranges |= set(
+                    range(
+                        x - abs(beacon["distance"] - distance),
+                        x + abs(beacon["distance"] - distance + 1),
+                    )
+                )
+
+        no_pos = len(ranges)
+        for beacon in {b["beacon"] for _, b in self.data.items()}:
+            if beacon[1] == row:
+                print(f"removing beacon: {beacon=}")
+                no_pos -= 1
+
+        return no_pos
 
     @timer(part=2)
     def solve_part_2(self, limit=4000000):
         """Solution for part 2"""
-        if self.filename == "test_input.txt":
-            limit = 20
-
-        for (x, y), beacon in self.data.items():
-            if ((0 <= (y - beacon['distance']) <= limit)
-                    and (0 <= (y + beacon['distance']) <= limit)
-                    and (0 <= (x - beacon['distance']) <= limit)
-                    and (0 <= (x + beacon['distance']) <= limit)):
-                yy = list(range(y - beacon['distance'], y + beacon['distance'] + 1))
-                xx = beacon['distance'] - abs(yy[yy.index(row)] - y)
-                for dx in range(-xx, xx + 1):
-                    pass
         return None
