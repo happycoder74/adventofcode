27734857027181abb29bfb40a206bc450ad0af2f
Christian Lundh <christianlundh1974@gmail.com>
2022-12-29 02:07:44 +0100

    Added some type hints

--- python/y2022/d20/aoc_2022_20.py
+++ python/y2022/d20/aoc_2022_20.py
@@ -1,27 +1,26 @@
 from collections import deque
-from common import timer, Puzzle
+from typing import Iterable
+
+from common import Puzzle, timer
 
 
 class Day20(Puzzle, year=2022, day=20):
-    def __init__(self, filename=None, data=None):
-        super().__init__(filename=filename, data=data)
-
     @staticmethod
-    def clean_input(data):
+    def clean_input(data: Iterable[str]) -> list[int]:
         return [int(d) for d in data]
 
-    def mix(self, rounds=1):
-        length = len(self.data)
+    def mix(self, rounds=1) -> int:
+        length: int = len(self.data)
         zero = (0, self.data.index(0))
         data = deque(
-            [(number, index) for index, number in enumerate(self.data)]
+                [(number, index) for index, number in enumerate(self.data)]
         )
         order = [n for n in self.data]
         for _ in range(rounds):
             for i, number in enumerate(order):
                 index = data.index((number, i))
                 data.rotate(-index)
-                value = data.popleft()
+                value: tuple[int, int] = data.popleft()
                 data.rotate(-value[0])
                 data.appendleft(value)
 
@@ -29,13 +28,13 @@ class Day20(Puzzle, year=2022, day=20):
         return sum(data[(zero_index + i) % length][0] for i in (1000, 2000, 3000))
 
     @timer(part=1)
-    def solve_part_1(self):
+    def solve_part_1(self) -> int:
         """Solution for part 1"""
         return self.mix()
 
     @timer(part=2)
-    def solve_part_2(self):
+    def solve_part_2(self) -> int:
         """Solution for part 2"""
-        factor = 811589153
+        factor: int = 811589153
         self.data = [n*factor for n in self.data]
         return self.mix(rounds=10)
