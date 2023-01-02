1fddd1fa97c82b3797b39991dc7180011e20f63d
Christian <christian@strandkarr.com>
2023-01-02 15:27:40 +0100

    fixed some type casting from malloc / calloc

--- C/lib/aoc_grid.c
+++ C/lib/aoc_grid.c
@@ -1,10 +1,9 @@
-#include <glib.h>
 #include <stdlib.h>
 #include <aoc_grid.h>
 
 Grid *grid_new(int rows, int columns) {
-    Grid *grid = malloc(sizeof(Grid));
-    grid->grid = calloc(sizeof(int),  rows * columns);
+    Grid *grid = (Grid *)malloc(sizeof(Grid));
+    grid->grid = (int *)calloc(sizeof(int), rows * columns);
     grid->rows = rows;
     grid->columns = columns;
 
@@ -20,4 +19,3 @@ void grid_index_set(Grid *grid, int row, int column, int value) {
     int index = row * grid->columns + column;
     grid->grid[index] = value;
 }
-
