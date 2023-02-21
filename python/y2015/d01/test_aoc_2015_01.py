2115569593f9ab2872da0855919174876ef6065f
Christian Lundh <christianlundh1974@gmail.com>
2023-02-21 18:21:03 +0100

    Cleanup and fixes

--- python/y2015/d01/test_aoc_2015_01.py
+++ python/y2015/d01/test_aoc_2015_01.py
@@ -1,15 +1,15 @@
 import unittest
-from aoc_2015_01 import Day1
+from aoc_2015_01 import Day01
 
 
 class Test_2015_01(unittest.TestCase):
     def setUp(self):
-        self.day = Day1(filename="input.txt")
+        pass
 
     def test_part_1(self):
-        self.day.data = ')())())'
-        self.assertEqual(-3, self.data.solve_part_1())
+        self.day = Day01(data=[')())())'])
+        self.assertEqual(-3, self.day.solve_part_1())
 
     def test_part_2(self):
-        self.data.data = '()())'
-        self.assertEqual(5, self.data.solve_part_2())
+        self.day = Day01(data=['()())'])
+        self.assertEqual(5, self.day.solve_part_2())
