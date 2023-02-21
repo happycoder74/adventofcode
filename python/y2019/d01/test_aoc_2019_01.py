bd83f32d5744a34edb25d4c154bf5436add135f2
Christian Lundh <christianlundh1974@gmail.com>
2023-02-21 18:23:53 +0100

    Type hinting and small fixes

--- python/y2019/d01/test_aoc_2019_01.py
+++ python/y2019/d01/test_aoc_2019_01.py
@@ -8,8 +8,18 @@ class Test_2019_01(unittest.TestCase):
             "100756"
         ]
 
-    def test_part_1(self):
+    def test_clean_input(self) -> None:
+        self.assertEqual([100756], Day.clean_input(self.data))
+
+    def test_calc_fuel_negative(self) -> None:
+        self.assertEqual(0, Day.calc_fuel(0))
+
+    def test_part_1(self) -> None:
         self.assertEqual(33583, Day(data=self.data).solve_part_1())
 
-    def test_part_2(self):
+    def test_part_2(self) -> None:
         self.assertEqual(50346, Day(data=self.data).solve_part_2())
+
+
+if __name__ == "__main__":
+    Test_2019_01().run()
