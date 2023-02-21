caa2caac0ee2e0a1849f73ac7271531934a53ab0
Christian Lundh <christianlundh1974@gmail.com>
2023-02-21 18:16:29 +0100

    Added type hinting

--- python/common/timer.py
+++ python/common/timer.py
@@ -1,11 +1,15 @@
 import time
 from functools import wraps
 
+from typing import Callable, Any
 
-def timer(part, show_result=True, title="", show_return=True):
-    def decorator(f):
+TFunc = Callable[..., Any]
+
+def timer(part: str|int, show_result: bool = True, title: str = "", 
+          show_return: bool = True) -> Callable[..., TFunc]:
+    def decorator(f: TFunc) -> TFunc:
         @wraps(f)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args, **kwargs) -> Any:
             start_time = time.perf_counter()
             result = f(*args, **kwargs)
             elapsed_time = time.perf_counter() - start_time
@@ -34,14 +38,14 @@ class Timer(object):
     title: str
     start_time: float
 
-    def __init__(self, title: str = None) -> None:
+    def __init__(self, title: str|None = None) -> None:
         self.start_time = 0
         self.title = f"{title} " if title is not None else ''
 
     def __enter__(self) -> None:
         self.start_time = time.perf_counter()
 
-    def __exit__(self, *info) -> None:
+    def __exit__(self, *info: list[Any]) -> None:
         elapsed_time: float = time.perf_counter() - self.start_time
         elapsed_time_unit: str = "ms" if elapsed_time <= 0.1 else "s"
         elapsed_time = elapsed_time * (1e3 if elapsed_time <= 0.1 else 1)
