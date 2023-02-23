70634b84ffa8a6d14f3919a15bebdda1372133de
Christian Lundh <christian.lundh@gknaerospace.com>
2023-02-23 13:41:12 +0100

    Updated to Puzzle class

--- python/y2021/d09/test_aoc_2021_09.py
+++ python/y2021/d09/test_aoc_2021_09.py
@@ -1,15 +1,13 @@
 import unittest
-import aoc_2021_09 as a
+from aoc_2021_09 import Day09 as Day
 
 
 class Test_2021_09(unittest.TestCase):
     def setUp(self):
-        self.data = a.clean_input(
-            a.get_input("test_input.txt", 2021, 9)
-        )
+        self.day = Day(filename="test_input.txt")
 
     def test_part_1(self):
-        self.assertEqual(15, a.solve_part_1(self.data))
+        self.assertEqual(15, self.day.solve_part_1())
 
     def test_part_2(self):
-        self.assertEqual(1134, a.solve_part_2(self.data))
+        self.assertEqual(1134, self.day.solve_part_2())
