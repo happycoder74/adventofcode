2b01d07e8658c2baa1254999e46cd47baa1963ff
Christian Lundh <christian.lundh@gknaerospace.com>
2023-02-24 14:44:29 +0100

    Updated

--- python/y2021/d18/aoc_2021_18.py
+++ python/y2021/d18/aoc_2021_18.py
@@ -1,5 +1,4 @@
 import itertools
-import sys
 from common import timer, Puzzle
 from SnailFishNumber import SnailfishNumber
 
@@ -23,15 +22,9 @@ class Day18(Puzzle, year=2021, day=18):
         magnitudes = []
         numbers = self.data
         for i, j in itertools.permutations(range(len(self.data)), 2):
-            magnitudes.append(SnailfishNumber.magnitude(
+            magnitudes.append(
+                SnailfishNumber.magnitude(
                     SnailfishNumber(numbers[i]) + SnailfishNumber(numbers[j])
                 )
             )
         return max(magnitudes)
-
-
-if __name__ == "__main__":
-    filename = "input.txt"
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-    Day18(filename=filename).solve_all()
--- python/y2021/d18/test_aoc_2021_18.py
+++ python/y2021/d18/test_aoc_2021_18.py
@@ -4,8 +4,18 @@ from aoc_2021_18 import Day18, SnailfishNumber
 
 class Test_2021_18(unittest.TestCase):
     def setUp(self):
-        Solver = Day18(filename="input.txt")
-        self.filedata = Solver.data
+        self.testdata = [
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]",
+        ]
         self.numbers = [
             [[1, 2], [[3, 4], 5]],
             [[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]],
@@ -26,9 +36,15 @@ class Test_2021_18(unittest.TestCase):
             self.assertEqual(answer, SnailfishNumber.magnitude(SN))
 
     def test_add(self):
-        number = (
-            SnailfishNumber("[[[[4,3],4],4],[7,[[8,4],9]]]") +
-            SnailfishNumber("[1,1]"))
+        number = SnailfishNumber(
+            "[[[[4,3],4],4],[7,[[8,4],9]]]"
+        ) + SnailfishNumber("[1,1]")
 
         answer = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
         self.assertEqual(answer, SnailfishNumber.to_string(number))
+
+    def test_part_1(self):
+        self.assertEqual(4140, Day18(data=self.testdata).solve_part_1())
+
+    def test_part_2(self):
+        self.assertEqual(3993, Day18(data=self.testdata).solve_part_2())
