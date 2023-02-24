293dd43af303806155165b7a9cedbe047aeb0923
Christian <christian@strandkarr.com>
2023-02-24 21:49:09 +0100

    Update

--- python/common/timer.py
+++ python/common/timer.py
@@ -6,16 +6,17 @@ def timer(part, show_result=True, title="", show_return=True):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            position = 35
             start_time = time.perf_counter()
             result = f(*args, **kwargs)
             elapsed_time = time.perf_counter() - start_time
             if elapsed_time < 0.1:
                 elapsed_time_str = (
-                    f"\r\033[35C( {(elapsed_time * 1e3):6.3f} ms )"
+                    f"\r\033[{position}C( {(elapsed_time * 1e3):6.3f} ms )"
                 )
             else:
                 elapsed_time_str = (
-                    f"\r\033[35C( {elapsed_time:6.3f} s  )"
+                    f"\r\033[{position}C( {elapsed_time:6.3f} s  )"
                 )
 
             if show_result:
