770e927c29f3fe181874b489e895a2e9aa449a0b
Christian Lundh <christian.lundh@gknaerospace.com>
2023-09-16 16:27:28 +0200

    Fixed solution for day 3

--- C/2019/03/aoc_2019_03.c
+++ C/2019/03/aoc_2019_03.c
@@ -94,78 +94,81 @@ void *solve_part_1(AocData_t *data) {
     return g_strdup_printf("%d", min_distance);
 }
 
+GArray *set_intersection(GHashTable *table1, GHashTable *table2) {
+    GArray *result = g_array_new(TRUE, TRUE, sizeof(Point *));
+    gpointer *keys;
+    Point *key;
+    guint length;
+
+    keys = g_hash_table_get_keys_as_array(table1, &length);
+    for(guint i = 0; i < length; i++) {
+        key = keys[i];
+        if(g_hash_table_contains(table2, key)) {
+            g_array_append_val(result, key);
+        }
+    }
+    return result;
+}
+
 void *solve_part_2(AocData_t *data) {
-    guint i, j;
-    GArray *intersection_points = g_array_new(TRUE, TRUE, sizeof(Point));
-    GArray *lines1 = g_array_index(data->data, GArray *, 0);
-    GArray *lines2 = g_array_index(data->data, GArray *, 1);
-    Line line1, line2;
-    gint min_steps = INT_MAX;
+    GArray *lines[2];
+    GHashTable *line_coords[2];
+    GHashTable *line_stepmap[2];
+    int signal = 0, min_signal = INT_MAX;
+    GArray *intersections;
+ 
+    lines[0] = g_array_index(data->data, GArray *, 0);
+    lines[1] = g_array_index(data->data, GArray *, 1);
 
-    for (i = 0; i < lines1->len; i++) {
-        for(j = 0; j < lines2->len; j++) {
-            Point intersection_point;
-            line1 = g_array_index(lines1, Line, i);
-            line2 = g_array_index(lines2, Line, j);
+    line_coords[0] = g_hash_table_new_full(point_hash, point_equal, g_free, NULL);
+    line_coords[1] = g_hash_table_new_full(point_hash, point_equal, g_free, NULL);
 
-            if(line_intersection(line1, line2, &intersection_point)) {
-                #ifndef NDEBUG
-                printf("(%d, %d)\n", intersection_point.x, intersection_point.y);
-                #endif
-                g_array_append_val(intersection_points, intersection_point);
+    // Using the same keys as line_coords why no free function is needed
+    line_stepmap[0] = g_hash_table_new_full(point_hash, point_equal, NULL, NULL);
+    line_stepmap[1] = g_hash_table_new_full(point_hash, point_equal, NULL, NULL);
+
+    for (guint wire = 0; wire < 2; wire++) {
+        Point *curr = point_new_m(0, 0);
+        guint steps = 0;
+        Point step;
+
+        for (guint i = 0; i < lines[wire]->len; i++) {
+            Line line = g_array_index(lines[wire], Line, i);
+            guint length = line_length(line);
+            if (is_horisontal(line))  {
+                step.x = (line.p1.x - line.p0.x) / (int)length;
+                step.y = 0;
+            } else {
+                step.x = 0;
+                step.y = (line.p1.y - line.p0.y) / (int)length;
+            }
+            for (guint j = 0; j < length; j++) {
+                curr = point_new_m(curr->x + step.x, curr->y + step.y);
+                steps += 1;
+                g_hash_table_insert(line_stepmap[wire], curr, GINT_TO_POINTER(steps));
+                g_hash_table_add(line_coords[wire], curr); 
             }
         }
+
     }
 
-    for (i = 1; i < intersection_points->len; i++) {
-        Line line;
-        int steps1 = 0, steps2 = 0;
-        int nsteps;
-        // Point test_point;
-        Point intersection_point = g_array_index(intersection_points, Point, i);
+    // g_hash_table_foreach(line_coords[0], print_coords, NULL);
+    intersections = set_intersection(line_coords[0], line_coords[1]);
 
-        printf("Intersection point (%d, %d)\n", intersection_point.x, intersection_point.y);
-        for (j = 0; j < lines1->len; j++) {
-            line = g_array_index(lines1, Line, j);
-            bool is_on_line = point_on_line(intersection_point, line);
-            if(!is_on_line) {
-                nsteps = point_manhattan_distance(line.p0, line.p1);
-#ifndef NDEBUG
-                printf("nsteps = %d (%d, %d) -> (%d, %d)\n", nsteps, line.p0.x, line.p0.y, line.p1.x, line.p1.y);
-                #endif
-                steps1 += nsteps;
-            } else {
-                nsteps = point_manhattan_distance(line.p0, intersection_point);
-                #ifndef NDEBUG
-                line_print(line);
-                printf("nsteps = %d (%d, %d) -> (%d, %d) (IP)\n", nsteps, line.p0.x, line.p0.y, intersection_point.x, intersection_point.y);
-                #endif
-                steps1 += nsteps;
-                break;
-            }
-        }
-        for (j = 0; j < lines2->len; j++) {
-            line = g_array_index(lines2, Line, j);
-            if(!point_on_line(intersection_point, line)) {
-                nsteps = point_manhattan_distance(line.p0, line.p1);
-                #ifndef NDEBUG
-                printf("nsteps = %d (%d, %d) -> (%d, %d)\n", nsteps, line.p0.x, line.p0.y, line.p1.x, line.p1.y);
-                #endif
-                steps2 += nsteps;
-            } else {
-                nsteps = point_manhattan_distance(line.p0, intersection_point);
-                #ifndef NDEBUG
-                line_print(line);
-                printf("nsteps = %d (%d, %d) -> (%d, %d) (IP)\n", nsteps, line.p0.x, line.p0.y, intersection_point.x, intersection_point.y);
-                #endif
-                steps2 += nsteps;
-                break;
-            }
-        }
-        min_steps = MIN((steps1 + steps2), min_steps);
+    for (guint i = 0; i < intersections->len; i++) {
+        Point *intersection_point = g_array_index(intersections, Point *, i);
+
+        signal = GPOINTER_TO_INT(g_hash_table_lookup(line_stepmap[0], intersection_point)) + 
+                 GPOINTER_TO_INT(g_hash_table_lookup(line_stepmap[1], intersection_point));
+        min_signal = MIN(signal, min_signal);
     }
-    
-    return g_strdup_printf("%d", min_steps);
+
+    g_hash_table_destroy(line_coords[0]);
+    g_hash_table_destroy(line_coords[1]);
+    g_hash_table_destroy(line_stepmap[0]);
+    g_hash_table_destroy(line_stepmap[1]);
+
+    return g_strdup_printf("%d", min_signal);
 }
 
 void *solve_all(AocData_t *data) {
