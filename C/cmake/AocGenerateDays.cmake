8f5bdce737f954466145dd631b60103bad803ba8
Christian <christian@strandkarr.com>
2023-09-21 09:26:39 +0200

    Added -lm which seems to be required on Linux

--- C/cmake/AocGenerateDays.cmake
+++ C/cmake/AocGenerateDays.cmake
@@ -4,9 +4,10 @@ function (AOC_GENERATE YEAR DAY)
 	target_include_directories(aoc_${YEAR}_${DAY} PRIVATE ${GLIB_INCLUDE_DIRS})
 	target_include_directories(aoc_${YEAR}_${DAY} PRIVATE ${CMAKE_SOURCE_DIR}/include)
 	target_link_libraries(aoc_${YEAR}_${DAY} ${GLIB_LDFLAGS})
-	target_link_libraries(aoc_${YEAR}_${DAY} 
+	target_link_libraries(aoc_${YEAR}_${DAY}
 		aoc
 		glib-2.0
+        m
 		)
 	target_compile_options(
 		aoc_${YEAR}_${DAY}
@@ -24,13 +25,13 @@ endfunction()
 function(AOC_GENERATE_YEAR AOC_YEAR)
 	set(DAY_DIRECTORIES
 		01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-		)	
+		)
 	set(custom_target_args)
 	foreach (DIR ${DAY_DIRECTORIES})
 		if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${DIR})
 			AOC_GENERATE(${AOC_YEAR} ${DIR})
-			list(APPEND custom_target_args COMMAND ${AOC_YEAR}/aoc_${AOC_YEAR}_${DIR}) 
-			list(APPEND custom_target_args DEPENDS aoc_${AOC_YEAR}_${DIR}) 
+			list(APPEND custom_target_args COMMAND ${AOC_YEAR}/aoc_${AOC_YEAR}_${DIR})
+			list(APPEND custom_target_args DEPENDS aoc_${AOC_YEAR}_${DIR})
 		else()
 			message("Skipping ${DIR}")
 		endif()
