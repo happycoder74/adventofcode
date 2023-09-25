b87b7106b19e24211d101c78de410fe13644c168
Christian Lundh <christian.lundh@gknaerospace.com>
2023-09-25 12:02:47 +0200

    Updated C template

--- templates/C/aoc_year_day.c
+++ templates/C/aoc_year_day.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <libgen.h>
 #include "aoc_utils.h"
 #include "aoc_timer.h"
 #include "aoc_array.h"
@@ -20,8 +19,6 @@ void *solve_part_2(AocData_t *data) {
 
 void *solve_all(AocData_t *data) {
 
-    data->data = clean_input(get_input(data->filename, data->year, data->day));
-
     if (data->data) {
         timer_func(1, solve_part_1, data, 1);
         timer_func(2, solve_part_2, data, 1);
@@ -33,16 +30,16 @@ void *solve_all(AocData_t *data) {
 int main(int argc, char **argv) {
     AocData_t *data;
 
-    char sourcefile[100];
+    char sourcefile[20];
     int year, day;
 
-    strcpy(sourcefile, basename(__FILE__));
+    strcpy(sourcefile, aoc_basename(__FILE__));
     sscanf(sourcefile, "aoc_%4d_%02d.c", &year, &day);
 
     if (argc > 1) {
-        data = aoc_data_new_clean(argv[1], year, day, NULL);
+        data = aoc_data_new_clean(argv[1], year, day, clean_input);
     } else {
-        data = aoc_data_new("input.txt", year, day);
+        data = aoc_data_new_clean("input.txt", year, day, clean_input);
     }
 
     printf("================================================\n");
