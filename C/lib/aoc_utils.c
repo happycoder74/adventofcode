b005a3c2e75d635611e04c8cc60bf2f62deb77df
Christian Lundh <christian.lundh@gknaerospace.com>
2023-09-25 16:53:28 +0200

    Removed duplicate function definitions

--- C/lib/aoc_utils.c
+++ C/lib/aoc_utils.c
@@ -169,42 +169,6 @@ AocArrayPtr get_input(char *filename, int year, int day) {
     return data;
 }
 
-gint int_array_max(gint *arr, gint length) {
-    gint max = arr[0];
-    gint i;
-
-    for (i = 1; i < length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    return max;
-}
-
-gint int_array_min(gint *arr, gint length) {
-    gint min = arr[0];
-    gint i;
-
-    for (i = 1; i < length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
-
-int min_non_zero(int *arr, int length) {
-    int min = length;
-    int i;
-
-    for (i = 0; i < length; i++) {
-        if ((arr[i] < min) && (arr[i] != 0)) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
-
 gint max(gint *arr, gint length) {
     gint max = arr[0];
     gint i;
