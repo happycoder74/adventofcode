c143b032d5651083f1b3ceb9b7efa517d2741d16
Christian Lundh <christian.lundh@gknaerospace.com>
2023-09-28 15:36:56 +0200

    Updated to fix legacy solutions

--- C/include/aoc_utils.h
+++ C/include/aoc_utils.h
@@ -4,8 +4,10 @@
 #include <stdbool.h>
 #include "aoc_types.h"
 //
-// aoc_timer.h included for legacy support
+// aoc_timer.h and aoc_array.h included for legacy support
+// will be removed when all solutions are converted
 #include "aoc_timer.h"
+#include "aoc_array.h"
 
 // AoC Data Structure macros
 #define aoc_data_new(filename, year, day)       (aoc_data_new_clean(filename, year, day, NULL))
@@ -14,11 +16,9 @@
 #define aoc_data_has_data(arr)                  ((arr)->data != NULL ? 1 : 0)
 
 // AoC Data Structure functions
-// size_t aoc_data_length(AocData_t *data);
-//GArray *aoc_data_data(AocData_t *data);
-AocData_t *aoc_data_set_data(AocData_t *aoc, GArray *data);
+AocData_t *aoc_data_set_data(AocData_t *aoc, AocArrayPtr data);
 void aoc_data_free(AocData_t *data);
-AocData_t *aoc_data_new_clean(gchar *filename, int year, int day, GArray *(*clean_function)(GArray *));
+AocData_t *aoc_data_new_clean(gchar *filename, int year, int day, AocArrayPtr (*clean_function)(AocArrayPtr ));
 
 bool is_horisontal(Line);
 bool is_vertical(Line);
@@ -30,22 +30,23 @@ Line line_new(Point, Point);
 Point *point_new_m(int, int);
 Point point_new(int, int);
 void point_print(Point);
-void line_array_print(GArray *lines);
+void line_array_print(AocArrayPtr lines);
 int line_length(Line);
 
 Point *line_intersection(Line line1, Line line2, Point *intersection_point);
 Point point_difference(Point, Point);
-guint point_hash(gconstpointer);
-gboolean point_equal(gconstpointer, gconstpointer);
+guint point_hash(const void *);
+int point_equal(const void *, const void *);
 int point_manhattan_distance(Point, Point);
 int point_distance(Point, Point);
 bool point_on_line(Point p, Line line);
 
 GSList *get_input_list(char *filename, int year, int day);
-GArray * get_input(char *filename, int year, int day);
-GArray * get_input_new(char *filename, int year, int day);
+AocArrayPtr get_input(char *filename, int year, int day);
+AocArrayPtr get_input_new(char *filename, int year, int day);
 
 // Legacy support functions
+// Returns max and min value in integer array
 int max(int *, int);
 int min(int *, int);
 // End of legacy support functions
