fca40389cc25f5e833e031b13d4883ee53b018aa
Christian Lundh <christian.lundh@gknaerospace.com>
2023-10-16 19:44:40 +0200

    Started rework with aoc_int32_hash_table

--- C/include/aoc_hash.h
+++ C/include/aoc_hash.h
@@ -5,13 +5,36 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef uint64_t (*hashfunction) (const char *key, size_t);
-typedef struct _aoc_hash_table aoc_hash_table;
+#include "aoc_types.h"
 
-aoc_hash_table *aoc_hash_table_create(uint32_t size, hashfunction hf);
-void aoc_hash_table_destroy(aoc_hash_table *ht);
-bool aoc_hash_table_insert(aoc_hash_table *ht, const char *key, void *obj);
-void *aoc_hash_table_lookup(aoc_hash_table *ht, const char *key);
-void *aoc_hash_table_delete(aoc_hash_table *ht, const char *key);
+typedef uint64_t (*str_hashfunction) (const char *key, size_t);
+typedef uint64_t (*ptr_hashfunction) (const void *key, size_t);
+typedef uint64_t (*int32_hashfunction) (const int32_t key);
+typedef uint64_t (*int64_hashfunction) (const int64_t key, size_t);
+typedef uint64_t (*uint32_hashfunction) (const uint32_t key, size_t);
+typedef uint64_t (*uint64_hashfunction) (const uint64_t key, size_t);
+typedef uint64_t (*point_hashfunction) (const Point key, size_t);
+typedef uint64_t (*line_hashfunction) (const Line key, size_t);
+
+typedef struct _aoc_hash_table AocHashTable;
+typedef AocHashTable * AocHashTablePtr;
+
+AocHashTablePtr aoc_int32_hash_table_create(uint32_t size, int32_hashfunction hf);
+void *aoc_int32_hash_table_lookup(AocHashTablePtr ht, const int32_t key);
+void aoc_int32_hash_table_destroy(AocHashTablePtr ht);
+bool aoc_int32_hash_table_insert(AocHashTablePtr ht, const int32_t key, const void *obj);
+void *aoc_int32_hash_table_delete(AocHashTablePtr ht, const int32_t key);
+
+AocHashTablePtr aoc_int64_hash_table_create(uint32_t size, int64_hashfunction hf);
+void *aoc_int64_hash_table_lookup(AocHashTablePtr ht, const int64_t key);
+
+AocHashTablePtr aoc_uint32_hash_table_create(uint32_t size, uint32_hashfunction hf);
+AocHashTablePtr aoc_uint64_hash_table_create(uint32_t size, uint64_hashfunction hf);
+AocHashTablePtr aoc_str_hash_table_create(uint32_t size, str_hashfunction hf);
+AocHashTablePtr aoc_ptr_hash_table_create(uint32_t size, ptr_hashfunction hf);
+
+void aoc_hash_table_destroy(AocHashTablePtr ht);
+bool aoc_hash_table_insert(AocHashTablePtr ht, const void *key, void *obj);
+void *aoc_hash_table_delete(AocHashTablePtr ht, const void *key);
 
 #endif /* AOC_HASH_H */
--- C/lib/aoc_hash.c
+++ C/lib/aoc_hash.c
@@ -5,117 +5,143 @@
 #include <stddef.h>
 
 typedef struct entry {
-    char *key;
-    void *object;
     struct entry *next;
+    void *object;
 } entry;
-        
+
+typedef struct {
+    struct entry head;
+    int32_t key;
+} int32_entry;
+
+typedef struct {
+    struct entry head;
+    int64_t key;
+} int64_entry;
+
+typedef struct {
+    struct entry head;
+    uint32_t key;
+} uint32_entry;
+
+typedef struct {
+    struct entry head;
+    uint64_t key;
+} uint64_entry;
+
+typedef enum {
+    AOC_HASH_INT32,
+    AOC_HASH_UINT32,
+    AOC_HASH_INT64,
+    AOC_HASH_UINT64,
+    AOC_HASH_COUNT
+} AocHashType;
 
 struct _aoc_hash_table {
+    AocHashType type;
     uint32_t size;
-    hashfunction hash;
     entry **elements;
 };
 
+typedef struct {
+    struct _aoc_hash_table head;
+    int32_hashfunction hash;
+} AocInt32HashTable;
 
-static size_t aoc_hash_table_index(aoc_hash_table *ht, const char *key) {
-    size_t result = (ht->hash(key, strlen(key)) % ht->size);
+static size_t aoc_int32_hash_table_index(AocHashTable *hashtable, const int32_t key) {
+    AocInt32HashTable *ht = (AocInt32HashTable *)hashtable;
+    size_t result = (ht->hash(key) % ht->head.size);
     return result;
 }
-aoc_hash_table *aoc_hash_table_create(uint32_t size, hashfunction hf) {
-    aoc_hash_table *ht = (aoc_hash_table *)malloc(sizeof(*ht));
-    ht->size = size;
+
+AocHashTablePtr aoc_int32_hash_table_create(uint32_t size, int32_hashfunction hf) {
+    AocInt32HashTable *ht = (AocInt32HashTable *)malloc(sizeof(*ht));
+    ht->head.size = size;
     ht->hash = hf;
 
-    ht->elements = calloc(sizeof(entry *), ht->size);
-    return ht;
+    ht->head.elements = calloc(sizeof(entry *), ht->head.size);
+    return (AocHashTablePtr) ht;
 }
 
-void aoc_hash_table_destroy(aoc_hash_table *ht) {
+void aoc_int32_hash_table_destroy(AocHashTable *hashtable) {
+    AocInt32HashTable *ht = (AocInt32HashTable *)hashtable;
+
     if (ht) {
-      if (ht->elements) {
-        free(ht->elements);
+      if (ht->head.elements) {
+        free(ht->head.elements);
       }
 
       free(ht);
     }
 }
 
-bool aoc_hash_table_insert(aoc_hash_table *ht, const char *key, void *obj) {
+bool aoc_int32_hash_table_insert(AocHashTable *ht, const int32_t key, const void *obj) {
     if (!ht)
         return false;
 
-    if (aoc_hash_table_lookup(ht, key))
+    if (aoc_int32_hash_table_lookup(ht, key))
         return false;
 
-    if (key == NULL || obj == NULL)
+    if (!obj) {
         return false;
+    }
 
-    size_t index  = aoc_hash_table_index(ht, key);
+    size_t index  = aoc_int32_hash_table_index(ht, key);
 
-    if (aoc_hash_table_lookup(ht, key) != NULL) return false;
+    if (aoc_int32_hash_table_lookup(ht, key) != NULL) return false;
 
     // create a new entry
 
-    entry *e = malloc(sizeof(*e));
-    e->object = obj;
-    memcpy(e->key, key, strlen(key) + 1);
+    int32_entry *e = malloc(sizeof(*e));
+    e->head.object = (void *)obj;
+    e->key = key;
 
     // insert our entry
-    e->next = ht->elements[index];
-    ht->elements[index] = e;
+    e->head.next = ht->elements[index];
+    ht->elements[index] = (entry *)e;
     return true;
 }
 
-void *aoc_hash_table_lookup(aoc_hash_table *ht, const char *key) {
+void *aoc_int32_hash_table_lookup(AocHashTablePtr ht, const int32_t key) {
     if (!ht)
         return false;
 
-    if (aoc_hash_table_lookup(ht, key))
-        return false;
+    size_t index  = aoc_int32_hash_table_index(ht, key);
 
-    if (key == NULL)
-        return false;
-
-    size_t index  = aoc_hash_table_index(ht, key);
-
-    entry *tmp = ht->elements[index];
-    while (tmp != NULL && strcmp(tmp->key, key) != 0) {
-        tmp = tmp->next;
+    int32_entry *tmp = (int32_entry *)ht->elements[index];
+    while (tmp != NULL && (tmp->key != key)) {
+        tmp = (int32_entry *)tmp->head.next;
     }
     if (tmp == NULL) return NULL;
-    return tmp->object;
+    return tmp->head.object;
 }
 
-void *aoc_hash_table_delete(aoc_hash_table *ht, const char *key) {
+void *aoc_int32_hash_table_delete(AocHashTablePtr ht, const int32_t key) {
     if (!ht)
         return false;
 
-    if (aoc_hash_table_lookup(ht, key))
+    if (aoc_int32_hash_table_lookup(ht, key))
         return false;
 
-    if (key == NULL)
-        return false;
+    size_t index  = aoc_int32_hash_table_index(ht, key);
 
-    size_t index  = aoc_hash_table_index(ht, key);
+    int32_entry *tmp = (int32_entry *)ht->elements[index];
+    int32_entry *prev = NULL;
 
-    entry *tmp = ht->elements[index];
-    entry *prev = NULL;
-
-    while (tmp != NULL && strcmp(tmp->key, key) != 0) {
+    while (tmp != NULL && (tmp->key != key)) {
         prev = tmp;
-        tmp = tmp->next;
+        tmp = (int32_entry *)tmp->head.next;
     }
     if (tmp == NULL) return NULL;
 
     if (prev == NULL) {
         // deleting the head of the list
-        ht->elements[index] = tmp->next;
+        ht->elements[index] = tmp->head.next;
     } else {
         // deleting elsewhere
-        prev->next = tmp->next;
+        prev->head.next = tmp->head.next;
     }
-    void *result = tmp->object;
+    void *result = tmp->head.object;
     free(tmp);
     return result;
 }
