4f6e6aff8f9060c608b983030a265da95c3bc2a3
Christian Lundh <christian.lundh@gknaerospace.com>
2023-10-18 16:19:05 +0200

    tested a git pre-commit hook with clang-format

--- C++/lib/aoc_io.cpp
+++ C++/lib/aoc_io.cpp
@@ -5,10 +5,9 @@
 
 namespace aoc {
 
-
 std::string get_input_bare(std::string fn, int year, int day) {
     std::ifstream ifs(fn);
-    std::string line;
+    std::string   line;
 
     std::getline(ifs, line);
 
@@ -16,10 +15,10 @@ std::string get_input_bare(std::string fn, int year, int day) {
 }
 
 namespace string {
-std::vector <std::string> split (const std::string& str, char delimiter) {
-    std::vector <std::string> tokens;
-    std::string token;
-    std::istringstream str_stream(str);
+std::vector<std::string> split(const std::string &str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string              token;
+    std::istringstream       str_stream(str);
 
     while (std::getline(str_stream, token, delimiter)) {
         tokens.push_back(token);
@@ -27,6 +26,6 @@ std::vector <std::string> split (const std::string& str, char delimiter) {
     return tokens;
 }
 
-} // namespace aoc::string
+} // namespace string
 
 } // namespace aoc
