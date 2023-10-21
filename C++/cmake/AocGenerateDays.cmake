b5777bc379fd36fae51bffcae0815b7872360396
Christian Lundh <christian.lundh@gknaerospace.com>
2023-10-21 08:58:03 +0200

    clang-format formatted

--- C++/cmake/AocGenerateDays.cmake
+++ C++/cmake/AocGenerateDays.cmake
@@ -3,7 +3,7 @@ function (AOC_GENERATE YEAR DAY)
 		${DAY}/aoc_${YEAR}_${DAY}.cpp
         )
 	target_include_directories(aoc_${YEAR}_${DAY} PRIVATE ${CMAKE_SOURCE_DIR}/include)
-	target_link_libraries(aoc_${YEAR}_${DAY} 
+	target_link_libraries(aoc_${YEAR}_${DAY}
 		aoc++
 		)
 	target_compile_options(
@@ -21,13 +21,13 @@ endfunction()
 function(AOC_GENERATE_YEAR AOC_YEAR)
 	set(DAY_DIRECTORIES
 		01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-		)	
+		)
 	set(custom_target_args)
 	foreach (DIR ${DAY_DIRECTORIES})
 		if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${DIR})
 			AOC_GENERATE(${AOC_YEAR} ${DIR})
-			list(APPEND custom_target_args COMMAND ${AOC_YEAR}/aoc_${AOC_YEAR}_${DIR}) 
-			list(APPEND custom_target_args DEPENDS aoc_${AOC_YEAR}_${DIR}) 
+			list(APPEND custom_target_args COMMAND ${AOC_YEAR}/aoc_${AOC_YEAR}_${DIR})
+			list(APPEND custom_target_args DEPENDS aoc_${AOC_YEAR}_${DIR})
 		else()
 			message("Skipping ${DIR}")
 		endif()
