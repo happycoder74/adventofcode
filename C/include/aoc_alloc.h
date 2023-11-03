24498bb30ce6c23ba11ae79406e6ea68b7d9d4ff
Christian Lundh <christian.lundh@gknaerospace.com>
2023-11-03 23:09:12 +0100

    Improved logging functionality

--- C/include/aoc_alloc.h
+++ C/include/aoc_alloc.h
@@ -3,11 +3,29 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-void    *aoc_malloc(size_t size);
-void    *aoc_calloc(size_t n_elements, size_t element_size);
-void    *aoc_realloc(void *mem_ptr, size_t new_size);
-void     aoc_free(void *mem_ptr);
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#ifndef NDEBUG
+#define aoc_malloc(size_)          aoc_malloc_internal(size_, __FUNCTION__, __FILENAME__, __LINE__)
+#define aoc_calloc(n_elem_, size_) aoc_calloc_internal(n_elem_, size_, __FUNCTION__, __FILENAME__, __LINE__)
+#define aoc_realloc(ptr_, size_)   aoc_realloc_internal(ptr_, size_, __FUNCTION__, __FILENAME__, __LINE__)
+#define aoc_free(ptr_)             aoc_free_internal(ptr_, __FUNCTION__, __FILENAME__, __LINE__)
+
+void    *aoc_malloc_internal(size_t size, const char *function, const char *file, const int line);
+void    *aoc_calloc_internal(size_t n_elements, size_t element_size, const char *function, const char *file, const int line);
+void    *aoc_realloc_internal(void *mem_ptr, size_t new_size, const char *function, const char *file, const int line);
+void     aoc_free_internal(void *mem_ptr, const char *function, const char *file, const int line);
 uint64_t aoc_mem_gc(void);
 
+#else
+
+#define aoc_malloc(size_)          malloc(size_)
+#define aoc_calloc(n_elem_, size_) calloc(n_elem_, size_)
+#define aoc_realloc(ptr_, size_)   realloc(ptr_, size_)
+#define aoc_free(ptr_)             free(ptr_)
+#define aoc_mem_gc()               0
+
+#endif
 #endif // !__AOC_ALLOC_H_
--- C/lib/aoc_alloc.c
+++ C/lib/aoc_alloc.c
@@ -1,5 +1,6 @@
 #include "aoc_hash.h"
 #include "glib.h"
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,13 +12,16 @@ static void init_mem_table(void) {
 }
 
 static void gc_free(void *key, void *value, void *user_data) {
+#ifdef DEBUG_VERBOSE
     fprintf(stderr, "gc:      %p (size = %u)\n", value, g_hash_table_size(mem_table));
+    fflush(stderr);
+#endif
 
-    // free(value);
+    free(value);
     return;
 }
 
-void *aoc_malloc(size_t size) {
+void *aoc_malloc_internal(size_t size, const char *function, const char *file, int line) {
     if (!mem_table)
         init_mem_table();
 
@@ -27,12 +31,14 @@ void *aoc_malloc(size_t size) {
 
     /* Add addr to hash_table here */
     g_hash_table_add(mem_table, addr);
-    fprintf(stderr, "malloc:  %p (size = %u)\n", addr, g_hash_table_size(mem_table));
-
+#ifdef DEBUG_VERBOSE
+    fprintf(stderr, "malloc:  %p (size = %u) - %s - %s:%d\n", addr, g_hash_table_size(mem_table), function, file, line);
+    fflush(stderr);
+#endif
     return addr;
 }
 
-void *aoc_calloc(size_t n_elements, size_t element_size) {
+void *aoc_calloc_internal(size_t n_elements, size_t element_size, const char *function, const char *file, int line) {
     void *addr = calloc(n_elements, element_size);
 
     if (!addr)
@@ -40,29 +46,36 @@ void *aoc_calloc(size_t n_elements, size_t element_size) {
 
     /* Add addr to hash_table here */
     g_hash_table_add(mem_table, addr);
-    fprintf(stderr, "calloc:  %p (size = %u)\n", addr, g_hash_table_size(mem_table));
+#ifdef DEBUG_VERBOSE
+    fprintf(stderr, "calloc:  %p (size = %u) - %s - %s:%d\n", addr, g_hash_table_size(mem_table), function, file, line);
+    fflush(stderr);
+#endif
 
     return addr;
 }
 
-void *aoc_realloc(void *orig, size_t new_size) {
-
+void *aoc_realloc_internal(void *orig, size_t new_size, const char *function, const char *file, int line) {
     g_hash_table_remove(mem_table, orig);
     void *addr = realloc(orig, new_size);
 
     if (addr) {
         /* Add addr to hash_table here */
         g_hash_table_add(mem_table, addr);
-        fprintf(stderr, "realloc: %p (size = %u)\n", addr, g_hash_table_size(mem_table));
+#ifdef DEBUG_VERBOSE
+        fprintf(stderr, "realloc: %p (size = %u) - %s - %s:%d\n", addr, g_hash_table_size(mem_table), function, file, line);
+        fflush(stderr);
+#endif
     }
 
     return addr;
 }
 
-void aoc_free(void *ptr) {
-    /* Delete ptr from mem_table if present */
+void aoc_free_internal(void *ptr, const char *function, const char *file, int line) { /* Delete ptr from mem_table if present */
     g_hash_table_remove(mem_table, ptr);
-    fprintf(stderr, "free:    %p (size = %u)\n", ptr, g_hash_table_size(mem_table));
+#ifdef DEBUG_VERBOSE
+    fprintf(stderr, "free:    %p (size = %u) - %s - %s:%d\n", ptr, g_hash_table_size(mem_table), function, file, line);
+    fflush(stderr);
+#endif
 
     // Free the memory
     free(ptr);
@@ -75,11 +88,16 @@ void aoc_free(void *ptr) {
 }
 
 uint64_t aoc_mem_gc(void) {
+
     if (!(mem_table) || (g_hash_table_size(mem_table) == 0)) {
         return 0;
     }
 
     uint64_t size = g_hash_table_size(mem_table);
+    if (size) {
+        fprintf(stderr, "%" PRIu64 " elements remaining\n", size);
+        fflush(stderr);
+    }
     g_hash_table_foreach(mem_table, gc_free, NULL);
     g_hash_table_destroy(mem_table);
     mem_table = NULL;
