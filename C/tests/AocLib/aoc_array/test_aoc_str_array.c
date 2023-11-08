af2497cdc7981d8f990e4a8f2be22ce37d094c76
Christian Lundh <christian.lundh@gknaerospace.com>
2023-11-08 22:12:10 +0100

    Added tests

---
+++ C/tests/AocLib/aoc_array/test_aoc_str_array.c
@@ -0,0 +1,76 @@
+#include "aoc_array.h"
+#include "aoc_types.h"
+#include "criterion/internal/assert.h"
+#include <criterion/criterion.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+AocArrayPtr array = NULL;
+
+void aoc_array_setup(void) {
+    array = aoc_str_array_new();
+}
+
+void aoc_array_teardown(void) {
+    free(array);
+}
+
+TestSuite(aoc_array, .init = aoc_array_setup, .fini = aoc_array_teardown);
+
+Test(aoc_array, test_str_array_new) {
+    cr_expect(array != NULL, "Expected new array to not be NULL");
+}
+
+Test(aoc_array, test_str_array_new_length) {
+    cr_expect(array->length == 0, "Expected new array length to be 0");
+}
+
+Test(aoc_array, test_str_array_append_string_literal) {
+    aoc_str_array_append(array, "Test String");
+    char **data = (char **)aoc_array_get_data(array);
+    cr_expect(!strcmp(data[0], "Test String"), "Expected value to be \"Test String\"");
+}
+
+Test(aoc_array, test_str_array_index) {
+    aoc_str_array_append(array, "One");
+    aoc_str_array_append(array, "Two");
+    aoc_str_array_append(array, "Three");
+
+    char *data = aoc_str_array_index(array, 1);
+    cr_expect(!strcmp(data, "Two"), "Expected value to be \"Two\"");
+}
+
+Test(aoc_array, test_str_array_remove_index) {
+    aoc_str_array_append(array, "One");
+    aoc_str_array_append(array, "Two");
+    aoc_str_array_append(array, "Three");
+
+    AocArrayPtr res = NULL;
+    res = aoc_array_remove_index(array, 1);
+
+    cr_assert_not_null(res);
+    cr_expect(!strcmp(aoc_str_array_index(array, 0), "One"), "Expected value to be \"One\"");
+    cr_expect(!strcmp(aoc_str_array_index(array, 1), "Three"), "Expected value to be \"Three\"");
+    cr_expect(aoc_array_length(array) == 2, "Expected a length of 2");
+}
+
+Test(aoc_array, test_str_array_prepend_to_empty) {
+    char str[] = "Test String Prepend";
+    array = aoc_str_array_prepend(array, str);
+
+    cr_assert_not_null(array);
+    cr_expect_eq(array->length, 1, "String length should be 1");
+    cr_expect_str_eq(aoc_str_array_index(array, 0), str, "Strings not equal");
+}
+
+Test(aoc_array, test_str_array_prepend_to_existing) {
+    char str[] = "Test String Prepend";
+    array = aoc_str_array_append(array, "First string (append)");
+    array = aoc_str_array_prepend(array, str);
+
+    cr_assert_not_null(array);
+    cr_expect_eq(array->length, 2, "String length should be 1");
+    cr_expect_str_eq(aoc_str_array_index(array, 0), str, "Strings not equal");
+    cr_expect_str_eq(aoc_str_array_index(array, 1), "First string (append)", "Strings not equal");
+}
---
+++ C/tests/AocLib/aoc_hash/test_aoc_int32_hash_table.c
@@ -0,0 +1,100 @@
+#include "aoc_hash.h"
+#include <criterion/criterion.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+AocHashTablePtr hash_table = NULL;
+AocHashTablePtr hash_table2 = NULL;
+int32_t        *keys = NULL;
+int32_t        *values = NULL;
+
+static uint64_t aoc_int_hash(int32_t key) {
+    return key;
+}
+
+static void aoc_int32_hash_table_load_N(size_t size) {
+    if (!hash_table)
+        return;
+
+    keys = (int32_t *)calloc(size, sizeof(int32_t));
+    values = (int32_t *)calloc(size, sizeof(int32_t));
+
+    for (size_t i = 0; i < size; i++) {
+        keys[i] = i + 1;
+        values[i] = i * 100;
+        aoc_int32_hash_table_insert(hash_table, keys[i], &values[i]);
+    }
+}
+
+void aoc_int32_hash_table_load_6(void) {
+    aoc_int32_hash_table_load_N(6);
+}
+
+void aoc_int32_hash_table_setup(void) {
+    hash_table = aoc_int32_hash_table_create(3, aoc_int_hash);
+}
+
+void aoc_int32_hash_table_teardown(void) {
+    if (hash_table)
+        free(hash_table);
+
+    if (keys)
+        free(keys);
+    if (values)
+        free(values);
+}
+
+TestSuite(aoc_int32_hash_table, .init = aoc_int32_hash_table_setup, .fini = aoc_int32_hash_table_teardown);
+
+Test(aoc_int32_hash_table, test_int32_hash_table_create) {
+    cr_expect_not_null(hash_table, "Expected new hash_table to not be NULL");
+}
+
+Test(aoc_int32_hash_table, test_int32_hash_table_insert_lookup) {
+    int32_t *value = (int32_t *)malloc(sizeof(int32_t));
+    *value = 5;
+    aoc_int32_hash_table_insert(hash_table, 30, value);
+    value = (int32_t *)malloc(sizeof(int32_t));
+    *value = 10;
+    aoc_int32_hash_table_insert(hash_table, 20, value);
+
+    int32_t *data = (int *)aoc_int32_hash_table_lookup(hash_table, 30);
+    int32_t  expected = 5;
+    int32_t  actual = *data;
+    cr_expect_eq(actual, expected, "Expected value to be [%d] but got [%d]", (int)expected, (int)actual);
+
+    data = (int *)aoc_int32_hash_table_lookup(hash_table, 20);
+    expected = 10;
+    actual = *data;
+    cr_expect_eq(actual, expected, "Expected value to be [%d] but got [%d]", (int)expected, (int)actual);
+
+    data = (int *)aoc_int32_hash_table_lookup(hash_table, 30);
+    expected = 5;
+    actual = *data;
+    cr_expect_eq(actual, expected, "Expected value to be [%d] but got [%d]", (int)expected, (int)actual);
+}
+
+Test(aoc_int32_hash_table, test_int32_hash_table_delete) {
+    int32_t *value = (int32_t *)malloc(sizeof(int32_t));
+    *value = 5;
+    aoc_int32_hash_table_insert(hash_table, 30, value);
+    value = (int32_t *)malloc(sizeof(int32_t));
+    *value = 10;
+    aoc_int32_hash_table_insert(hash_table, 20, value);
+
+    int32_t *return_value = (int32_t *)aoc_int32_hash_table_delete(hash_table, 30);
+    cr_expect_not_null(return_value, "Did not expect return value from delete to be NULL");
+    cr_expect_eq(5, *return_value, "Expected %d, but got %d", 5, *return_value);
+}
+
+Test(aoc_int32_hash_table, test_int32_hash_table_rehash, .init = aoc_int32_hash_table_load_6) {
+    int32_t value = 1001;
+    aoc_int32_hash_table_insert(hash_table, 100, &value);
+    int32_t expected = 17;
+    int32_t actual = (int32_t)aoc_hash_table_size(hash_table);
+    cr_expect_eq(expected, actual, "Expected size of %d, but got %d", expected, actual);
+    expected = values[4];
+    actual = *(int32_t *)aoc_int32_hash_table_lookup(hash_table, keys[4]);
+    cr_expect_eq(expected, actual, "Expected value of key: %d to be %d but got %d", keys[4], expected, actual);
+}
