9023d438a02b9915c565318b2d7a1c7f486eff98
Christian Lundh <christian.lundh@gknaerospace.com>
2023-11-20 05:51:05 +0100

    New test for str_split

--- C/tests/AocLib/aoc_string/test_aoc_string.c
+++ C/tests/AocLib/aoc_string/test_aoc_string.c
@@ -16,14 +16,18 @@ void aoc_string_setup(void) {
 void aoc_string_with_whitespace_setup(void) {
     char str_literal_with_ws[] = "   Test string   ";
     if (str_dynamic) {
-        str_dynamic = (char *)realloc(str_dynamic, sizeof(char) * (strlen(str_literal_with_ws) + 1));
+        char *new_ptr = (char *)realloc(str_dynamic, sizeof(char) * (strlen(str_literal_with_ws) + 1));
+        if (new_ptr) {
+            str_dynamic = new_ptr;
+        }
+        strcpy(str_dynamic, str_literal_with_ws);
     }
-    strcpy(str_dynamic, str_literal_with_ws);
 }
 
 void aoc_string_teardown(void) {
-    if (str_dynamic)
+    if (str_dynamic) {
         free(str_dynamic);
+    }
 }
 
 TestSuite(aoc_string, .init = aoc_string_setup, .fini = aoc_string_teardown);
@@ -44,4 +48,21 @@ Test(aoc_string, test_string_str_trim_all_whitespace, .init = aoc_string_with_wh
     char *actual = str_trim(str_dynamic);
 
     cr_expect_str_eq(actual, expected, "Expected '%s' but got '%s", expected, actual);
+}
+
+Test(aoc_string, test_str_split) {
+    char  *test_string = "Here I Come";
+    char **result = str_split(test_string, " ", 0);
+
+    cr_expect_str_eq(result[0], "Here", "Expected something else, got %s", result[0]);
+    cr_expect_str_eq(result[1], "I", "Expected something else, got %s", result[1]);
+    cr_expect_str_eq(result[2], "Come", "Expected something else, got %s", result[2]);
+}
+
+Test(aoc_string, test_str_split_max_tokens) {
+    char  *test_string = "Here I Come";
+    char **result = str_split(test_string, " ", 2);
+
+    cr_expect_str_eq(result[0], "Here", "Expected something else, got %s", result[0]);
+    cr_expect_str_eq(result[1], "I Come", "Expected something else, got %s", result[1]);
 }
