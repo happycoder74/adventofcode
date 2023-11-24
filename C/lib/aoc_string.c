e2f3a86605a5669590519855337f2bbc79db3e05
Christian Lundh <christian.lundh@gknaerospace.com>
2023-11-24 18:27:40 +0100

    Using a aoc_ptr_array instead of a dynamic Char** array

--- C/lib/aoc_string.c
+++ C/lib/aoc_string.c
@@ -165,7 +165,6 @@ char *strdup_printf(const char *format, ...) {
 char **str_split(const char *str, const char *delimiter, uint32_t max_tokens) {
     char       *s = NULL;
     const char *remainder;
-
     AocArrayPtr return_split = NULL;
 
     if (max_tokens < 1) {
