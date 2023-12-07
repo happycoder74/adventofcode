1bef89d810aecb93d2539a110dd290fa3196c7fe
Christian Lundh <christian.lundh@gknaerospace.com>
2023-12-07 15:03:21 +0100

    Funny solution to 2023 day 4 part 1

---
+++ python/y2023/d04/aoc_2023_04.py
@@ -0,0 +1,35 @@
+from common import timer, Puzzle
+
+
+class Day04(Puzzle, year=2023, day=4):
+    @staticmethod
+    def clean_input(data):
+        return data
+
+    @timer(part=1)
+    def solve_part_1(self):
+        """Solution for part 1"""
+        return sum(
+            [
+                v
+                for v in [
+                    2
+                    ** (
+                        len(
+                            set(card[1].split()).intersection(
+                                set(card[0].split())
+                            )
+                        )
+                        - 1
+                    )
+                    for card in [d[9:].split("|") for d in self.data]
+                ]
+                if v >= 1
+            ]
+        )
+
+    @timer(part=2)
+    def solve_part_2(self):
+        """Solution for part 2"""
+        result = 0
+        return result
