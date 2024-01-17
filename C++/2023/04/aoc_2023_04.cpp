19f177b5e1a65433bc77b591d5bb4b34b838a6f8
Christian Lundh <christian.lundh@gknaerospace.com>
2024-01-17 20:05:52 +0100

    C++ Solution for 2023 day 4

--- C++/2023/04/aoc_2023_04.cpp
+++ C++/2023/04/aoc_2023_04.cpp
@@ -3,25 +3,21 @@
 #include "aoc_timer.hpp"
 #include <algorithm>
 #include <cmath>
+#include <map>
+#include <numeric>
 #include <ranges>
 
 using card = std::pair<std::vector<int>, std::vector<int>>;
-
-template <typename T> void vec_print(const std::vector<T> &vec, std::string title = "") {
-    std::cout << title << " { ";
-    for (auto &item : vec) {
-        std::cout << item << " ";
-    }
-    std::cout << "}" << std::endl;
-}
+using parsed_input = std::vector<card>;
 
 card split_deck(const std::string &row) {
     std::vector<std::string> res = aoc::string::split(row.substr(row.find(":") + 1), '|');
 
     std::vector<std::string> r1 = aoc::string::split(res.front(), ' ');
-    auto                     av1 = r1 | std::views::transform([](const auto &s) -> int { return std::stoi(s); });
     std::vector<std::string> r2 = aoc::string::split(res.back(), ' ');
-    auto                     av2 = r2 | std::views::transform([](const auto &s) -> int { return std::stoi(s, NULL); });
+
+    auto av1 = r1 | std::views::transform([](const auto &s) -> int { return std::stoi(s); });
+    auto av2 = r2 | std::views::transform([](const auto &s) -> int { return std::stoi(s, NULL); });
 
     std::vector<int> v1, v2;
 
@@ -37,12 +33,21 @@ card split_deck(const std::string &row) {
     return std::make_pair(v1, v2);
 }
 
-std::string solve_part_1(const std::vector<std::string> data) {
-    auto deck = data | std::views::transform([](const auto &s) -> card { return split_deck(s); });
-    int  sum = 0;
-    for (auto c : deck) {
-        std::vector<int> hand;
+std::vector<card> parse_input(const std::vector<std::string> data) {
+    std::vector<card> return_data;
+    auto              deck = data | std::views::transform([](const auto &s) -> card { return split_deck(s); });
 
+    for (auto item : deck) {
+        return_data.push_back(item);
+    }
+
+    return return_data;
+}
+
+std::string solve_part_1(parsed_input data) {
+    int sum = 0;
+    for (auto c : data) {
+        std::vector<int> hand;
         std::set_intersection(c.second.begin(), c.second.end(), c.first.begin(), c.first.end(), std::back_inserter(hand));
         int v = static_cast<int>(std::pow(2.0, hand.size() - 1));
         if (v >= 1) {
@@ -52,11 +57,26 @@ std::string solve_part_1(const std::vector<std::string> data) {
     return std::format("{}", sum);
 }
 
-std::string solve_part_2(std::vector<std::string> data) {
-    return std::format("{}", 0);
+std::string solve_part_2(parsed_input data) {
+    std::map<int, int> deck;
+
+    for (int i = 0; i < data.size(); i++) {
+        deck[i] += 1;
+        card             c = data[i];
+        std::vector<int> hand;
+        std::set_intersection(c.second.begin(), c.second.end(), c.first.begin(), c.first.end(), std::back_inserter(hand));
+        for (int j = 0; j < hand.size(); j++) {
+            deck[i + j + 1] += deck[i];
+        }
+    }
+    int sum = 0;
+    for (auto &item : deck) {
+        sum += item.second;
+    }
+    return std::format("{}", sum);
 }
 
-void *solve_all(std::vector<std::string> data) {
+void *solve_all(parsed_input data) {
 
     if (data.size() > 0) {
         aoc::timer(1, solve_part_1, data, 1);
@@ -85,7 +105,7 @@ int main(int argc, char **argv) {
 
     printf("================================================\n");
     printf("Solution for %d, day %02d\n", year, day);
-    aoc::timer(0, solve_all, data, 0);
+    aoc::timer(0, solve_all, parse_input(data), 0);
 
     return 0;
 }
