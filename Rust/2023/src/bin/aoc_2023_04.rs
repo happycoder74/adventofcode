a2c2c936267e5dc71b31e5421219ff90d1d6c49c
Christian Lundh <christian.lundh@gknaerospace.com>
2024-02-05 21:55:07 +0100

    Updated structure

--- Rust/2023/src/bin/aoc_2023_04.rs
+++ Rust/2023/src/bin/aoc_2023_04.rs
@@ -1,6 +1,11 @@
+use std::time::Instant;
 use std::collections::HashSet;
+use aoc_utils::input::get_input;
+use aoc_utils::types::AocReturn;
+use aoc_utils::output::report;
 
 type Cards = HashSet<i32>;
+
 fn parse_cards(input: &str) -> Cards {
     input
         .split_whitespace()
@@ -8,7 +13,6 @@ fn parse_cards(input: &str) -> Cards {
         .collect()
 }
 
-#[aoc_generator(day4)]
 pub fn parse_input(input: &str) -> Vec<(Cards, Cards)> {
     let mut numbers = vec![];
 
@@ -22,14 +26,28 @@ pub fn parse_input(input: &str) -> Vec<(Cards, Cards)> {
     numbers
 }
 
-#[aoc(day4, part1)]
-pub fn solve_part_1(input: &[(Cards, Cards)]) -> i32 {
-    input
+fn solve_part_1(input: &[(Cards, Cards)]) -> AocReturn {
+    let start_time = Instant::now();
+    let result: i32 = input
         .iter()
         .map(|(winning, hand)| winning.intersection(hand).count())
         .map(|n| match n {
             0 => 0,
             n => 1 << (n - 1),
         })
-        .sum()
+        .sum();
+    AocReturn {result, duration: Instant::now() - start_time}
+}
+
+fn solve_part_2(input: &[(Cards, Cards)]) -> AocReturn {
+    let start_time = Instant::now();
+    let result = input.len() as i32;
+    AocReturn {result, duration: Instant::now() - start_time}
+}
+
+pub fn main() {
+    let input = get_input(2023, 4);
+    let data = parse_input(&input);
+    report("Part 1", solve_part_1(&data));
+    report("Part 2", solve_part_2(&data));
 }
