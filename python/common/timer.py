6540974641a7679ddecc43959377be50ddc61d04
Christian Lundh <christian.lundh@gknaerospace.com>
2024-02-19 10:22:30 +0100

    Output formatting changes

--- python/common/timer.py
+++ python/common/timer.py
@@ -1,38 +1,39 @@
 import time
 from functools import wraps
-
-from typing import Callable, Any
+from typing import Any, Callable
 
 TFunc = Callable[..., Any]
 
 
-def timer(part: str | int, show_result: bool = True, title: str = "",
-          show_return: bool = True) -> Callable[..., TFunc]:
+def timer(
+    part: str | int, show_result: bool = True, title: str = "", show_return: bool = True
+) -> Callable[..., TFunc]:
     def decorator(f: TFunc) -> TFunc:
         @wraps(f)
         def wrapper(*args, **kwargs) -> Any:
-            position = 35
             start_time = time.perf_counter()
             result = f(*args, **kwargs)
             elapsed_time = time.perf_counter() - start_time
             if elapsed_time < 0.1:
-                elapsed_time_str = (
-                    f"\r\033[{position}C( {(elapsed_time * 1e3):6.3f} ms )"
-                )
+                elapsed_time_str = f"{(elapsed_time * 1e3):>20.3f} ms"
             else:
-                elapsed_time_str = (
-                    f"\r\033[{position}C( {elapsed_time:6.3f} s  )"
-                )
+                elapsed_time_str = f"{elapsed_time:>20.3f} s"
 
             if show_result:
                 header = title or f"Part {part} answer"
-                print("".join([
-                    f"{header}: ",
-                    f'{result if result is not None and show_return else ""}',
-                    elapsed_time_str])
+                print(
+                    "".join(
+                        [
+                            f"{header:<15}: ",
+                            f'{result if result is not None and show_return else "":<20}',
+                            elapsed_time_str,
+                        ]
+                    )
                 )
             return result
+
         return wrapper
+
     return decorator
 
 
@@ -42,7 +43,7 @@ class Timer(object):
 
     def __init__(self, title: str | None = None) -> None:
         self.start_time = 0
-        self.title = f"{title} " if title is not None else ''
+        self.title = f"{title} " if title is not None else ""
 
     def __enter__(self) -> None:
         self.start_time = time.perf_counter()
@@ -51,4 +52,4 @@ class Timer(object):
         elapsed_time: float = time.perf_counter() - self.start_time
         elapsed_time_unit: str = "ms" if elapsed_time <= 0.1 else "s"
         elapsed_time = elapsed_time * (1e3 if elapsed_time <= 0.1 else 1)
-        print(f'{self.title}{elapsed_time:.3f} {elapsed_time_unit}')
+        print(f"{self.title}{elapsed_time:.3f} {elapsed_time_unit}")
