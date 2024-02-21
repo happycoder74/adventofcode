43ae30c4cfff78ce5cbe57c98f96b1dab98dc680
Christian <christian@strandkarr.com>
2024-02-21 15:15:39 +0100

    Replaced GTimer with POSIX clock_gettime (Not very portable)

--- C/lib/aoc_timer.c
+++ C/lib/aoc_timer.c
@@ -1,8 +1,10 @@
+#define _XOPEN_SOURCE 600 // enable the clock_gettime functionality
 #include "aoc_timer.h"
 #include "aoc_alloc.h"
-#include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 typedef struct Duration {
     double duration;
@@ -13,17 +15,17 @@ Duration convert_duration(double elapsed) {
 
     Duration duration;
 
-    if (elapsed > 1) {
-        duration.duration = elapsed;
+    if (elapsed > 1e9) {
+        duration.duration = elapsed / 1e9;
         sprintf(duration.unit, "s");
-    } else if (elapsed > 1e-3) {
-        duration.duration = elapsed * 1e3;
+    } else if (elapsed > 1e6) {
+        duration.duration = elapsed / 1e6;
         sprintf(duration.unit, "ms");
-    } else if (elapsed > 1e-6) {
-        duration.duration = elapsed * 1e6;
+    } else if (elapsed > 1e3) {
+        duration.duration = elapsed / 1e3;
         sprintf(duration.unit, "\u03BCs");
     } else {
-        duration.duration = elapsed * 1e9;
+        duration.duration = elapsed;
         sprintf(duration.unit, "ns");
     }
 
@@ -31,21 +33,21 @@ Duration convert_duration(double elapsed) {
 }
 
 void timer_func(int part, void *(func)(AocData_t *), AocData_t *aocdata, gboolean show_res) {
-    gdouble elapsed, elapsed_unit;
-    GTimer *timer = g_timer_new();
-    char   *result = (char *)func(aocdata);
-    g_timer_stop(timer);
+    gdouble         elapsed, elapsed_unit;
+    struct timespec start, stop;
 
-    Duration duration = convert_duration(g_timer_elapsed(timer, NULL));
+    clock_gettime(CLOCK_REALTIME, &start);
+    char *result = (char *)func(aocdata);
+    clock_gettime(CLOCK_REALTIME, &stop);
+
+    Duration duration = convert_duration((stop.tv_sec * 1e9 + stop.tv_nsec) - (start.tv_sec * 1e9 + start.tv_nsec));
 
     if (show_res) {
-        printf("Part %d answer: %-20s%20.2lf %-2s\n", part, result, duration.duration, duration.unit);
-
+        printf("Part %d answer: %-20s%10.2lf %-2s\n", part, result, duration.duration, duration.unit);
     } else {
-        printf("Time elapsed : %40.2lf %-2s\n", duration.duration, duration.unit);
+        printf("Time elapsed : %30.2lf %-2s\n", duration.duration, duration.unit);
     }
 
-    g_timer_destroy(timer);
     if (result)
         aoc_free(result);
 }
