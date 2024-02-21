2892740a2795cd97a7d3bb6fe4e209c9b665237c
Christian Lundh <christian.lundh@gknaerospace.com>
2024-02-21 23:43:40 +0100

    2016 changed

--- C/2016/03/aoc_2016_03.c
+++ C/2016/03/aoc_2016_03.c
@@ -1,5 +1,6 @@
 #include "aoc_alloc.h"
 #include "aoc_array.h"
+#include "aoc_io.h"
 #include "aoc_string.h"
 #include "aoc_timer.h"
 #include "aoc_types.h"
@@ -45,8 +46,9 @@ void *solve_part_2(AocData_t *data) {
 
     for (j = 0; j < 3; j++) {
         for (i = 0; i < aoc_data_length(data) - 2; i += 3) {
-            if (is_valid_i(int_array[j][i], int_array[j][i + 1], int_array[j][i + 2]))
+            if (is_valid_i(int_array[j][i], int_array[j][i + 1], int_array[j][i + 2])) {
                 count++;
+            }
         }
     }
 
@@ -64,26 +66,13 @@ void *solve_all(AocData_t *data) {
 }
 
 int main(int argc, char **argv) {
-    AocData_t *data;
 
-    char sourcefile[20];
-    int  year, day;
+    const unsigned year = 2016;
+    const unsigned day = 3;
 
-    strcpy(sourcefile, aoc_basename(__FILE__));
-    sscanf(sourcefile, "aoc_%4d_%02d.c", &year, &day);
+    AocData_t *data = get_data(argc, argv, year, day, NULL);
 
-    if (argc > 1) {
-        if (!strncmp(argv[1], "--test", 6)) {
-            data = aoc_data_new("test_input.txt", year, day);
-        } else {
-            data = aoc_data_new(argv[1], year, day);
-        }
-    } else {
-        data = aoc_data_new("input.txt", year, day);
-    }
-
-    printf("================================================\n");
-    printf("Solution for %d, day %02d\n", year, day);
+    aoc_header(year, day);
     timer_func(0, solve_all, data, 0);
 
     aoc_data_free(data);
