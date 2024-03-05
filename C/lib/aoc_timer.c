891891f3ed27d86bb2d3d693b472f40a58d773d5
Christian Lundh <christian.lundh@gknaerospace.com>
2024-03-05 14:27:29 +0100

    First benchmark implementation

--- C/lib/aoc_timer.c
+++ C/lib/aoc_timer.c
@@ -56,6 +56,61 @@ void timer_func(int part, void *(func)(AocData_t *), AocData_t *aocdata, int sho
     }
 }
 
+void timer_func_bench(int part, void *(func)(AocData_t *), AocData_t *aocdata, int show_res, unsigned warmup, unsigned iterations) {
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    LARGE_INTEGER startTime;
+    QueryPerformanceCounter(&startTime);
+    LARGE_INTEGER endTime;
+
+    double *warmup_timings = NULL;
+    if (warmup >= 1) {
+       warmup_timings = (double *)calloc(warmup, sizeof(double));
+    }
+    double *benchmark_timings = NULL;
+    if (iterations >= 1) {
+       benchmark_timings = (double *)calloc(iterations, sizeof(double));
+    } else {
+        benchmark_timings = (double *)calloc(1, sizeof(double));
+    }
+
+    for (unsigned warmup_counter = 0; warmup_counter < warmup; warmup_counter++) {
+        QueryPerformanceCounter(&startTime);
+        char         *result = (char *)func(aocdata);
+        QueryPerformanceCounter(&endTime);
+        double   timeDifference = ((endTime.QuadPart - startTime.QuadPart) * 1e9 / freq.QuadPart);
+        warmup_timings[warmup_counter] = timeDifference;
+    }
+
+    char *result = NULL;
+    for (unsigned bench_counter = 0; bench_counter <= iterations; bench_counter++) {
+        QueryPerformanceCounter(&startTime);
+        result = (char *)func(aocdata);
+        QueryPerformanceCounter(&endTime);
+        double   timeDifference = ((endTime.QuadPart - startTime.QuadPart) * 1e9 / freq.QuadPart);
+        benchmark_timings[bench_counter] = timeDifference;
+    }
+
+    double meanTimeDifference = 0;
+    unsigned bench_counter;
+    for (bench_counter = 0; bench_counter <= iterations; bench_counter++) {
+        meanTimeDifference += benchmark_timings[bench_counter];
+    }
+    Duration duration = convert_duration(meanTimeDifference);
+    printf("%lf %-2s\n", duration.duration, duration.unit);
+    meanTimeDifference = meanTimeDifference / bench_counter;
+
+    duration = convert_duration(meanTimeDifference);
+    if (show_res) {
+        printf("Part %d answer: %-20s%10.2lf %-2s\n", part, result, duration.duration, duration.unit);
+    } else {
+        printf("Time elapsed : %30.2lf %-2s\n", duration.duration, duration.unit);
+    }
+
+    if (result) {
+        aoc_free(result);
+    }
+}
 #else
 #define _XOPEN_SOURCE 600
 void timer_func(int part, void *(func)(AocData_t *), AocData_t *aocdata, int show_res) {
