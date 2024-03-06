fbff0bcda0b0f0ee8b725f3ec0512ce44fd58aa4
Christian Lundh <christian.lundh@gknaerospace.com>
2024-03-06 15:26:53 +0100

    Initial commit for aoc_stack

---
+++ C/include/aoc_stack.h
@@ -0,0 +1,17 @@
+#ifndef __AOC_STACK_H__
+#define __AOC_STACK_H__
+
+#include <stdlib.h>
+
+typedef struct _AocStack AocStack;
+typedef AocStack        *AocStackPtr;
+
+AocStackPtr aoc_stack_new(void);
+
+void aoc_stack_push(AocStackPtr stack, void *item);
+
+void *aoc_stack_pop(AocStackPtr stack);
+
+void aoc_stack_free(AocStackPtr stack);
+
+#endif // !__AOC_STACK_H__
