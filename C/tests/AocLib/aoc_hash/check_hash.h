080ae0c88cffdd3bf0df16b56964402e2b7d5414
Christian Lundh <christian.lundh@gknaerospace.com>
2024-03-18 15:30:41 +0100

    Added aoc_hash_int32 tests

--- C/tests/AocLib/aoc_hash/check_hash.h
+++ C/tests/AocLib/aoc_hash/check_hash.h
@@ -9,5 +9,7 @@ TCase *test_case_aoc_hash_foreach(void);
 TCase *test_case_aoc_hash_buckets(void);
 TCase *test_case_aoc_hash_contains(void);
 TCase *test_case_aoc_hash_char(void);
+TCase *test_case_aoc_hash_int32(void);
+TCase *test_case_aoc_hash_int32_loaded(void);
 
 #endif /* end of include guard: CHECK_HASH_H_TGIHEEF4 */
