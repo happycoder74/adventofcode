1a68357017a31c15da3ae99bdec290e16eeb81d7
Christian Lundh <christian.lundh@gknaerospace.com>
2024-03-18 21:33:53 +0100

    Adding check tests for aoc_regex and removing old criterion test file

--- C/tests/AocLib/check_aoclib.c
+++ C/tests/AocLib/check_aoclib.c
@@ -3,6 +3,7 @@
 #include "aoc_array/check_array.h"
 #include "aoc_grid/check_grid.h"
 #include "aoc_hash/check_hash.h"
+#include "aoc_regex/check_regex.h"
 #include "aoc_sets/check_sets.h"
 #include "aoc_stack/check_stack.h"
 #include "aoc_string/check_aoc_string.h"
@@ -16,6 +17,7 @@ int main(void) {
     srunner_add_suite(runner, test_aoc_grid());
     srunner_add_suite(runner, test_aoc_string());
     srunner_add_suite(runner, test_aoc_stack());
+    srunner_add_suite(runner, test_aoc_regex());
 
     srunner_run_all(runner, CK_NORMAL);
 
