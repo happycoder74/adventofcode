097ec088cf23162bd743af11afee73cf0adc58a6
Christian Lundh <christian.lundh@gknaerospace.com>
2024-03-18 15:03:54 +0100

    Adding check tests for aoc_int64_array

--- C/tests/AocLib/aoc_array/check_array.c
+++ C/tests/AocLib/aoc_array/check_array.c
@@ -2,12 +2,13 @@
 #include <check.h>
 
 Suite *test_aoc_array(void) {
-    Suite *s = suite_create("test_aoc_array");
+    Suite *s = suite_create("aoc_array");
 
     suite_add_tcase(s, test_case_int32_array());
     suite_add_tcase(s, test_case_ptr_array());
     suite_add_tcase(s, test_case_double_array());
     suite_add_tcase(s, test_case_str_array());
+    suite_add_tcase(s, test_case_int64_array());
 
     return s;
 }
