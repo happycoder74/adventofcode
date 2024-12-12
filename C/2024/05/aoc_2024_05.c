78bbdd47d0706de2a22df016f81656ba329c893f
Christian (Lenovo) <christian@strandkarr.com>
2024-12-12 10:40:48 +0100

    C Solution for 2024 day 5
    Cleaned up a bit.

--- C/2024/05/aoc_2024_05.c
+++ C/2024/05/aoc_2024_05.c
@@ -1,71 +1,75 @@
-#include <ctype.h>
 #define _XOPEN_SOURCE 600 // To get hold of clock_gettime etc.
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+unsigned short int is_right_order(unsigned int table[100][100], unsigned int *update) {
+    unsigned short int right_order = 1;
+
+    unsigned int  node;
+    unsigned int *targets = NULL;
+
+    for (unsigned index = 1; (index <= update[0]) && (right_order); index++) {
+        node = update[index];
+        targets = table[node];
+
+        if (targets[0] == 0)
+            continue;
+
+        unsigned int target = 0;
+        unsigned     ii;
+
+        unsigned short int found;
+        for (ii = 1; (ii < index) && (!found); ii++) {
+            target = update[ii];
+            found = 0;
+            for (unsigned int target_index = 0; targets[target_index] != 0; target_index++) {
+                if (targets[target_index] == target) {
+                    found = 1;
+                    right_order = 0;
+                    break;
+                }
+            }
+        }
+    }
+    return right_order;
+}
+
+unsigned int reorder(unsigned int table[100][100], unsigned int *update) {
+    unsigned int  reordered_update[200] = {0};
+    unsigned int  return_update[200] = {0};
+    unsigned int *targets = NULL;
+
+    for (unsigned int index = 1; index <= update[0]; index++) {
+        unsigned int node = update[index];
+        targets = table[node];
+        for (unsigned int ii = 1; ii <= update[0]; ii++) {
+            for (unsigned int jj = 0; targets[jj] != 0; jj++) {
+                if (update[ii] == targets[jj]) {
+                    reordered_update[index] += 1;
+                }
+            }
+        }
+    }
+
+    for (unsigned int i = 1; i <= update[0]; i++) {
+        return_update[reordered_update[i] + 1] = update[i];
+    }
+
+    return return_update[(update[0] / 2) + 1];
+}
 
 int solve_part_1(unsigned int table[100][100], unsigned int updates[200][100],
                  unsigned int n_updates) {
 
     unsigned int  count = 0;
     unsigned int *update = NULL;
-    unsigned int *targets = NULL;
 
     for (unsigned int i_update = 0; i_update < n_updates; i_update++) {
-        short int    right_order = 1;
-        unsigned int node;
         update = updates[i_update];
-
-        for (unsigned index = 1; index <= update[0]; index++) {
-            node = update[index];
-            targets = table[node];
-
-            if (targets[0] == 0)
-                continue;
-
-            unsigned int target = 0;
-            unsigned int ii = index + 1;
-            for (unsigned jj = ii; jj <= update[0]; jj++) {
-                target = update[jj];
-                unsigned short int found = 0;
-                /* Check if update number is in node target list */
-                for (unsigned int target_index = 0; targets[target_index] != 0; target_index++) {
-                    if (targets[target_index] == target) {
-                        found = 1;
-                        break;
-                    }
-                }
-                if (!found) {
-                    right_order = 0;
-                    break;
-                }
-                ii++;
-            }
-            if (!right_order) {
-                break;
-            }
-            ii = 1;
-            for (unsigned jj = ii; jj < index; jj++) {
-                target = update[jj];
-                unsigned short int found = 0;
-                for (unsigned int target_index = 0; targets[target_index] != 0; target_index++) {
-                    if (targets[target_index] == target) {
-                        found = 1;
-                        break;
-                    }
-                }
-                if (found) {
-                    right_order = 0;
-                    break;
-                }
-                ii++;
-            }
-            if (!right_order) {
-                break;
-            }
-        }
-        if (right_order) {
+        if (is_right_order(table, update)) {
             count += update[(update[0] / 2) + 1];
         }
     }
@@ -73,8 +77,18 @@ int solve_part_1(unsigned int table[100][100], unsigned int updates[200][100],
     return count;
 }
 
-int solve_part_2(void) {
-    unsigned int count = 0;
+int solve_part_2(unsigned int table[100][100], unsigned int updates[200][100],
+                 unsigned int n_updates) {
+    unsigned int  count = 0;
+    unsigned int *update;
+    unsigned int  reordered_update[200] = {0};
+
+    for (unsigned int i_update = 0; i_update < n_updates; i_update++) {
+        update = updates[i_update];
+        if (!is_right_order(table, update)) {
+            count += reorder(table, update);
+        }
+    }
 
     return count;
 }
@@ -149,7 +163,7 @@ int main(int argc, char **argv) {
             (stop.tv_sec * 1e3 + stop.tv_nsec * 1e-6) - (start.tv_sec * 1e3 + start.tv_nsec * 1e-6),
             (stop.tv_nsec - start.tv_nsec));
     clock_gettime(CLOCK_REALTIME, &start);
-    fprintf(stdout, "Solution to part 2: %10d", solve_part_2());
+    fprintf(stdout, "Solution to part 2: %10d", solve_part_2(table, rules, line_counter));
     clock_gettime(CLOCK_REALTIME, &stop);
     fprintf(stdout, "%10.3lf ms (%lu ns)\n",
             (stop.tv_sec * 1e3 + stop.tv_nsec * 1e-6) - (start.tv_sec * 1e3 + start.tv_nsec * 1e-6),
