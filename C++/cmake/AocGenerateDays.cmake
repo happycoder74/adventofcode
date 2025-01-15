0f2286b31eaaa0c10e467ae6de83f4d90520a36e
Christian (Lenovo) <christian@strandkarr.com>
2025-01-15 11:57:39 +0100

    Moved 2015 source files from separate day directories
    Flattened the directory structure by removing the day directories
    and instead storing the all sources under each year instead

--- C++/cmake/AocGenerateDays.cmake
+++ C++/cmake/AocGenerateDays.cmake
@@ -40,3 +40,43 @@ function(AOC_GENERATE_YEAR AOC_YEAR)
 		)
 endfunction()
 
+function (AOC_GENERATE_NEW YEAR DAY)
+	add_executable(aoc_${YEAR}_${DAY}
+		aoc_${YEAR}_${DAY}.cpp
+        )
+	target_include_directories(aoc_${YEAR}_${DAY} PRIVATE ${CMAKE_SOURCE_DIR}/include)
+	target_link_libraries(aoc_${YEAR}_${DAY}
+		aoc++
+		)
+	add_custom_target(run_${YEAR}_${DAY}
+		COMMAND aoc_${YEAR}_${DAY}
+		DEPENDS aoc_${YEAR}_${DAY}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+		)
+    add_custom_target(test_${YEAR}_${DAY}
+        COMMAND aoc_${YEAR}_${DAY} --test
+        DEPENDS aoc_${YEAR}_${DAY}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        )
+endfunction()
+function(AOC_GENERATE_YEAR_NEW AOC_YEAR)
+	set(DAYS
+		01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+		)
+	set(custom_target_args)
+	foreach (DIR ${DAYS})
+		if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/aoc_${AOC_YEAR}_${DIR}.cpp)
+			AOC_GENERATE_NEW(${AOC_YEAR} ${DIR})
+			list(APPEND custom_target_args COMMAND ${AOC_YEAR}/aoc_${AOC_YEAR}_${DIR})
+			list(APPEND custom_target_args DEPENDS aoc_${AOC_YEAR}_${DIR})
+		else()
+			message("Skipping ${DIR}")
+		endif()
+	endforeach()
+
+	add_custom_target(
+		run_${AOC_YEAR}
+		${custom_target_args}
+		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build
+		)
+endfunction()
