5352765306caf3b219488962023de588dc2166ba
Christian Lundh <christian.lundh@gknaerospace.com>
2025-01-24 21:32:37 +0100

    C solution for 2020 day 13 both parts

--- C/2020/aoc_2020_13.c
+++ C/2020/aoc_2020_13.c
@@ -5,11 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+
+struct Bus {
+    uint32_t bus_id;
+    int32_t  offset;
+};
 
 struct Input {
-    uint32_t timestamp;
-    uint32_t bus_id_list[100];
-    size_t   bus_count;
+    uint32_t   timestamp;
+    struct Bus bus_list[100];
+    struct Bus bus_list_sorted[100];
+    size_t     bus_count;
 };
 
 int solve_part_1(void *inp) {
@@ -18,7 +25,7 @@ int solve_part_1(void *inp) {
 
     uint32_t min_wait = UINT32_MAX;
     for (size_t index = 0; index < input->bus_count; index++) {
-        uint32_t id = input->bus_id_list[index];
+        uint32_t id = input->bus_list[index].bus_id;
         uint32_t wait_time = (id - (input->timestamp % id));
         if (wait_time < min_wait) {
             min_wait = wait_time;
@@ -29,11 +36,31 @@ int solve_part_1(void *inp) {
     return result;
 }
 
-int solve_part_2(void *inp) {
+uint64_t solve_part_2(void *inp) {
     struct Input *input = (struct Input *)inp;
-    int           result = 0;
 
-    return result;
+    struct Bus *bus_list = input->bus_list;
+    struct Bus *bus = bus_list;
+
+    uint64_t timestamp = 0;
+    int      start = 1;
+    uint64_t step = bus->bus_id;
+    while (1) {
+        timestamp += step;
+        uint8_t valid = 1;
+        for (size_t bus_index = start; bus_index < input->bus_count; bus_index++) {
+            bus = bus_list + bus_index;
+            if ((timestamp + bus->offset) % bus->bus_id) {
+                valid = 0;
+                break;
+            }
+            start += 1;
+            step *= bus->bus_id;
+        }
+        if (valid) {
+            return timestamp;
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -69,16 +96,25 @@ int main(int argc, char **argv) {
     }
 
     input.bus_count = 0;
-    fgets(line, 254, fp);
+    if (!fgets(line, 254, fp)) {
+        fprintf(stderr, "Premature end of file\n");
+    }
     input.timestamp = atoi(line);
-    fgets(line, 254, fp);
-    char *token = strtok(line, ",");
+    if (!fgets(line, 254, fp)) {
+        fprintf(stderr, "Premature end of file\n");
+    }
+    char  *token = strtok(line, ",");
+    size_t index = 0;
     while (token) {
         if (strcmp(token, "x")) {
-            input.bus_id_list[input.bus_count++] = atoi(token);
+            input.bus_list[input.bus_count].bus_id = atoi(token);
+            input.bus_list[input.bus_count].offset = index;
+            input.bus_count++;
         }
+        index++;
         token = strtok(NULL, ",");
     }
+    memmove(&input.bus_list_sorted, &input.bus_list, input.bus_count * sizeof(struct Bus));
 
     fclose(fp);
 
@@ -87,7 +123,7 @@ int main(int argc, char **argv) {
     aoc_header(year, day);
     aoc_timer_gen("Preparation time:", timer, BORDER_BOTTOM);
     timer_func_new(1, solve_part_1, &input, 1);
-    timer_func_new(2, solve_part_2, &input, 1);
+    timer_func_uint64(2, solve_part_2, &input, 1, NULL);
     aoc_timer_stop(timer);
     aoc_timer_gen("Total time:", timer, BORDER_TOP | BORDER_BOTTOM);
 
