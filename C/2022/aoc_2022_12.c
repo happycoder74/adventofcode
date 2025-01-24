b5fb8e5b0ef8c3a3f8a3fcccc95498052577af2f
Christian Lundh <christian.lundh@gknaerospace.com>
2025-01-24 14:51:45 +0100

    Fixed compiler warning on OpenBSD

--- C/2022/aoc_2022_12.c
+++ C/2022/aoc_2022_12.c
@@ -173,7 +173,7 @@ void solve(Trail *trail) {
     }
 
     // After interrupt
-    printf("Queue length: %d\n", queue->length);
+    printf("Queue length: %zu\n", queue->length);
     Node *current = (Node *)aoc_array_last(queue);
     printf("Current point: (%d, %d)\n", current->state->x, current->state->y);
     return;
