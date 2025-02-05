dd13f62d9eee9dce883044f6a6077e30a4b08c9c
Christian (Lenovo) <christian@strandkarr.com>
2025-02-05 19:11:10 +0100

    Updated C++ solution for 2020 day 1

--- C++/2020/aoc_2020_01.cpp
+++ C++/2020/aoc_2020_01.cpp
@@ -5,33 +5,29 @@
 #include <span>
 #include <string>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
-struct Input {
-    std::vector<int>        data;
-    std::unordered_set<int> s;
-};
+int solve_part_1(const std::pair<std::vector<int>, std::unordered_set<int>> &data) {
+    constexpr int target_value = 2020;
 
-int solve_part_1(const struct Input &data) {
-    const int target_value = 2020;
-
-    for (auto &c : data.data) {
-        if (data.s.contains(target_value - c))
+    for (auto &c : data.first) {
+        if (data.second.contains(target_value - c))
             return c * (target_value - c);
     }
 
     return 0;
 }
 
-int solve_part_2(const struct Input &data) {
-    const int target_value = 2020;
+int solve_part_2(const std::pair<std::vector<int>, std::unordered_set<int>> &data) {
+    constexpr int target_value = 2020;
 
-    for (auto &num1 : data.data) {
+    for (auto &num1 : data.first) {
         auto remaining = target_value - num1;
-        for (auto &num2 : data.data) {
+        for (auto &num2 : data.first) {
             if (num2 > remaining)
                 continue;
-            if (data.s.contains(remaining - num2))
+            if (data.second.contains(remaining - num2))
                 return num1 * num2 * (remaining - num2);
         }
     }
@@ -40,13 +36,14 @@ int solve_part_2(const struct Input &data) {
 }
 
 int main(int argc, char **argv) {
-    std::string           filename;
     std::filesystem::path filepath(std::getenv("AOC_DATA_LOCATION"));
-    const int             year = 2020;
-    const int             day  = 1;
-    struct Input          data = {std::vector<int>(), std::unordered_set<int>()};
 
-    auto args = std::span(argv, size_t(argc));
+    std::string filename;
+    const int   year = 2020;
+    const int   day  = 1;
+
+    auto                                                 args = std::span(argv, size_t(argc));
+    std::pair<std::vector<int>, std::unordered_set<int>> data;
 
     if (argc > 1) {
         if (std::string(args[1]) == "--test") {
@@ -58,11 +55,12 @@ int main(int argc, char **argv) {
         filename = "input.txt";
     }
 
-    std::ifstream ifs(filepath / std::filesystem::path(std::format("{}/{:02d}", year, day)) / std::filesystem::path(filename));
+    filepath = filepath / std::format("{}/{:02d}", year, day) / filename;
+    std::ifstream ifs(filepath);
     std::string   line;
     while (std::getline(ifs, line)) {
-        data.data.push_back(std::stoi(line));
-        data.s.insert(stoi(line));
+        data.first.push_back(std::stoi(line));
+        data.second.insert(stoi(line));
     }
 
     aoc::timer(1, solve_part_1, data);
