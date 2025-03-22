c2c77bdd6931dd00a90771d7456a576ffedf59c6
Christian (Lenovo) <christian@strandkarr.com>
2025-03-23 00:06:51 +0100

    C++23 2016 day 1 (Update)

--- C++/2016/aoc_2016_01.cpp
+++ C++/2016/aoc_2016_01.cpp
@@ -1,6 +1,9 @@
 #include "aoc_io.hpp"
 #include "aoc_timer.hpp"
 #include <array>
+#include <exception>
+#include <iostream>
+#include <ranges>
 #include <unordered_set>
 
 enum class Part {
@@ -8,8 +11,14 @@ enum class Part {
     TWO
 };
 
+enum class Turn {
+    UNDEFINED,
+    LEFT,
+    RIGHT
+};
+
 using Point = std::pair<int, int>;
-using Step  = std::pair<char, int>;
+using Step  = std::pair<Turn, int>;
 
 template <>
 struct std::hash<std::pair<int, int>> {
@@ -18,12 +27,43 @@ struct std::hash<std::pair<int, int>> {
     }
 };
 
-auto manhattan_distance(const Point &p) -> unsigned int {
+Point operator+(const Point &l, const Point &r) {
+    return {l.first + r.first, l.second + r.second};
+}
+
+Point operator*(const Point &l, const int r) {
+    return {l.first * r, l.second * r};
+}
+
+inline auto manhattan_distance(const Point &p) -> int {
     return std::abs(p.first) + std::abs(p.second);
 }
 
-auto solution(const std::string &instructions, Part part) -> int {
-    std::vector<Step>         step;
+auto parse_instructions(const std::string &instructions) {
+    std::vector<Step> parsed{};
+    std::stringstream ss{instructions};
+
+    char turn_char = '\0';
+    int  steps     = 0;
+    Turn turn      = Turn::UNDEFINED;
+
+    while (!ss.fail()) {
+        ss >> turn_char >> steps;
+        switch (turn_char) {
+            case 'L':
+                turn = Turn::LEFT;
+                break;
+            case 'R':
+                turn = Turn::RIGHT;
+                break;
+        }
+        parsed.emplace_back(turn, steps);
+        ss.seekg(ss.tellg() + static_cast<std::stringstream::pos_type>(1));
+    }
+    return parsed;
+}
+
+auto solution(const std::vector<Step> &instructions, Part part) -> int {
     Point                     pos = {0, 0};
     std::unordered_set<Point> locations{pos};
 
@@ -31,53 +71,49 @@ auto solution(const std::string &instructions, Part part) -> int {
         {{0, 1}, {-1, 0}, {0, -1}, {1, 0}}
     };
 
-    std::stringstream ss{instructions};
-    char              turn  = '\0';
-    int               steps = 0;
-
-    while (!ss.fail()) {
-        ss >> turn >> steps;
-        switch (turn) {
-            case 'L':
+    for (const auto &step : instructions) {
+        switch (step.first) {
+            case Turn::LEFT:
                 std::rotate(directions.begin(), directions.begin() + 1, directions.end());
                 break;
-            case 'R':
+            case Turn::RIGHT:
                 std::rotate(directions.rbegin(), directions.rbegin() + 1, directions.rend());
                 break;
+            default:
+                std::cerr << "This should not happen (Undefined direction)" << '\n';
+                std::terminate();
         }
 
         auto current_direction = directions.front();
 
         if (part == Part::ONE) {
-            pos.first  = pos.first + current_direction.first * steps;
-            pos.second = pos.second + current_direction.second * steps;
+            pos = pos + current_direction * step.second;
         } else {
-            for (auto step = 1; step <= steps; ++step) {
-                pos.first += current_direction.first;
-                pos.second += current_direction.second;
+            for ([[maybe_unused]] auto s : std::views::iota(0) | std::views::take(step.second)) {
+                pos = pos + current_direction;
                 if (locations.contains(pos)) {
-                    return int(manhattan_distance(pos));
+                    return manhattan_distance(pos);
                 } else {
                     locations.insert(pos);
                 }
             }
         }
-        ss.seekg(ss.tellg() + static_cast<std::stringstream::pos_type>(1));
     }
-    return int(manhattan_distance(pos));
+    return manhattan_distance(pos);
 }
 
-auto solve_part_1(const std::string &instructions) -> int {
+auto solve_part_1(const std::vector<Step> &instructions) -> int {
     return solution(instructions, Part::ONE);
 }
 
-auto solve_part_2(const std::string &instructions) -> int {
+auto solve_part_2(const std::vector<Step> &instructions) -> int {
     return solution(instructions, Part::TWO);
 }
 
 void solve_all(const std::string &instructions) {
-    aoc::timer(1, solve_part_1, instructions);
-    aoc::timer(2, solve_part_2, instructions);
+    auto parsed_instructions = aoc::timer(parse_instructions, instructions, "Preparation time:");
+    aoc::timer(1, solve_part_1, parsed_instructions);
+    aoc::timer(2, solve_part_2, parsed_instructions);
 }
 
 auto main(int argc, char **argv) -> int {
