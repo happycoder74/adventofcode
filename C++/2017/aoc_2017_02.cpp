edb4fc8933b6f20429adb0befa0e26e51eb5bdf5
Christian (Lenovo) <christian@strandkarr.com>
2025-03-24 11:41:57 +0100

    Updates to more C++23-ish code

--- C++/2017/aoc_2017_02.cpp
+++ C++/2017/aoc_2017_02.cpp
@@ -9,7 +9,7 @@
 
 namespace aoc_2017_02 {
 
-auto transform_input(const std::vector<std::string> &data) {
+auto transform_input(const auto &data) {
 
     auto lines = data //
                  | std::views::transform([](const auto &dataline) {
@@ -25,63 +25,65 @@ auto transform_input(const std::vector<std::string> &data) {
     return lines;
 }
 
-auto solve_part_1(const std::vector<std::vector<int>> &lines) -> int {
+auto solve_part_1(const auto &lines) -> int {
 
     auto rng = lines //
                | std::views::transform([](const auto &line) {
                      auto [min, max] = std::ranges::minmax_element(line);
                      return *max - *min;
                  });
+
     auto checksum = std::reduce(rng.begin(), rng.end());
     return checksum;
 }
 
-auto solve_part_2(const std::vector<std::vector<int>> &lines) -> int {
+auto solve_part_2(const auto &lines) -> int {
 
     auto rng = lines //
                | std::views::transform([](const auto &line) {
-                     int checksum = 0;
-                     for (auto &num : line) {
-                         for (auto &nn : line) {
-                             if ((nn != num) && ((num % nn) == 0)) {
-                                 checksum += num / nn;
-                             }
-                         }
-                     }
-                     return checksum;
+                     auto rng2 = std::views::cartesian_product(line, line) //
+                                 | std::views::filter([](const auto &v) {
+                                       auto &[num1, num2] = v;
+                                       return ((num2 != num1) && ((num1 % num2) == 0));
+                                   }) //
+                                 | std::views::transform([](const auto &v) {
+                                       auto &[num1, num2] = v;
+                                       return num1 / num2;
+                                   });
+                     return std::reduce(rng2.begin(), rng2.end());
                  });
     auto result = std::reduce(rng.begin(), rng.end());
     return result;
 }
 
-void solve_all(const std::vector<std::string> &data) {
-    auto lines = aoc::timer(aoc_2017_02::transform_input, data, "Preparation time:");
+void solve_all(const auto &data) {
+    auto lines = aoc::timer(aoc_2017_02::transform_input<decltype(data)>, data, "Preparation time:");
 
-    aoc::timer(1, aoc_2017_02::solve_part_1, lines);
-    aoc::timer(2, aoc_2017_02::solve_part_2, lines);
+    aoc::timer(1, aoc_2017_02::solve_part_1<decltype(lines)>, lines);
+    aoc::timer(2, aoc_2017_02::solve_part_2<decltype(lines)>, lines);
 }
 } // namespace aoc_2017_02
 
 int main(int argc, char **argv) {
-    std::string filename;
-    const int   year = 2017;
-    const int   day  = 2;
-
-    std::vector<std::string> data;
+    std::string   filename;
+    constexpr int year = 2017;
+    constexpr int day  = 2;
 
     auto args = std::span(argv, argc);
     if (argc > 1) {
         if (std::string(args[1]) == "--test") {
-            data = aoc::io::get_input_list<std::string>("test_input.txt", year, day);
+            filename = "test_input.txt";
         } else {
-            data = aoc::io::get_input_list<std::string>(args[1], year, day);
+            filename = args[1];
         }
     } else {
-        data = aoc::io::get_input_list<std::string>("input.txt", year, day);
+        filename = "input.txt";
     }
 
+    auto data = aoc::io::get_input_list<std::string>(filename, year, day);
+
     aoc::io::header(year, day);
-    aoc::timer(aoc_2017_02::solve_all, data);
+    aoc::timer(aoc_2017_02::solve_all<decltype(data)>, data);
 
     return 0;
 }
