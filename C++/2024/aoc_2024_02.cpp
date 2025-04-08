1d34cdee961fb47a30147894d9804f01b778455a
Christian Lundh <christian.lundh@gknaerospace.com>
2025-04-08 16:02:32 +0200

    C++23 2024 day 2 (Updated solution)

--- C++/2024/aoc_2024_02.cpp
+++ C++/2024/aoc_2024_02.cpp
@@ -7,38 +7,36 @@
 #include <vector>
 
 auto parse_instructions(const std::vector<std::string> &instructions) {
-    auto return_vector = instructions | std::views::transform([](const auto &line) -> std::vector<int> {
-                             std::vector<int>  vec{};
-                             std::stringstream ss{line};
-                             std::string       str{};
-                             while (std::getline(ss, str, ' ')) {
-                                 vec.push_back(std::stoi(str));
-                             }
-                             return vec;
-                         }) |
-                         std::ranges::to<std::vector>();
+    auto return_vector = instructions //
+                         | std::views::transform([](const auto &line) -> std::vector<int> {
+                               std::vector<int>  vec{};
+                               std::stringstream ss{line};
+                               std::string       str{};
+                               while (std::getline(ss, str, ' ')) {
+                                   vec.push_back(std::stoi(str));
+                               }
+                               return vec;
+                           }) //
+                         | std::ranges::to<std::vector>();
     return return_vector;
 }
 
 auto validate(const std::vector<int> &vec) -> bool {
 
-    auto v2 = std::views::zip(vec, vec | std::views::drop(1)) | std::ranges::views::transform([](const auto &p) -> int {
-                  auto &[a, b] = p;
-                  return (b - a);
-              });
+    auto diffs = [](const auto &p) -> int {
+        auto &[a, b] = p;
+        return (b - a);
+    };
+    auto v2 = std::views::zip(vec, vec | std::views::drop(1)) | std::views::transform(diffs);
 
     auto is_ok = std::ranges::all_of(v2, [](const int a) -> bool {
         auto c = std::abs(a);
         return ((1 <= c) && (c <= 3));
     });
 
-    auto is_increasing = std::ranges::all_of(v2, [](const int a) -> bool {
-        return a > 0;
-    });
+    auto is_increasing = std::ranges::is_sorted(vec);
+    auto is_decreasing = std::ranges::is_sorted(vec | std::views::reverse);
 
-    auto is_decreasing = std::ranges::all_of(v2, [](const int a) -> bool {
-        return a < 0;
-    });
     return (is_ok && (is_increasing || is_decreasing));
 }
 
@@ -49,17 +47,11 @@ auto solve_part_1(const std::vector<std::vector<int>> &instructions) -> int {
 
 auto solve_part_2(const std::vector<std::vector<int>> &instructions) -> int {
     auto result = std::ranges::count_if(instructions, [](const std::vector<int> &vec) -> int {
-        if (validate(vec)) {
-            return true;
-        }
-        for (int i = 0; i < static_cast<int>(vec.size()); i++) {
-            std::vector<int> v1(vec.begin(), vec.begin() + i);
-            v1.insert(v1.end(), vec.begin() + i + 1, vec.end());
-            if (validate(v1)) {
-                return true;
-            }
-        }
-        return false;
+        return (validate(vec) || std::ranges::any_of(std::views::iota(0, static_cast<int>(vec.size())), [&vec](const auto i) {
+                    std::vector tmp_vec{vec};
+                    tmp_vec.erase(tmp_vec.begin() + i);
+                    return validate(tmp_vec);
+                }));
     });
     return static_cast<int>(result);
 }
