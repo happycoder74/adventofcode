31b2d0498d650029cab3bd3eb1ddc70a6c8c31a0
Christian Lundh <christian.lundh@gknaerospace.com>
2025-04-09 08:59:54 +0200

    Rust 2024 day 4

---
+++ Rust/2024/src/aoc_2024_04.rs
@@ -0,0 +1,107 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day4)]
+pub fn parse(input: &str) -> Vec<String> {
+    let lines: Vec<String> = input.lines().map(|m| String::from(m)).collect();
+    lines
+}
+
+#[aoc(day4, part1)]
+pub fn part1(input: &Vec<String>) -> u64 {
+    let mut result: u64 = 0;
+    let rows = input.len() as i32;
+    let cols = input[0].len() as i32;
+    for (row_index, row) in input.iter().enumerate() {
+        for (col_index, col) in row.char_indices() {
+            if col != 'X' {
+                continue;
+            }
+            for dr in -1..=1 {
+                for dc in -1..=1 {
+                    if dr == 0 && dc == 0 {
+                        continue;
+                    }
+                    if (row_index as i32 + 3 * dr) < 0 || rows <= (row_index as i32 + 3 * dr) {
+                        continue;
+                    }
+                    if (col_index as i32 + 3 * dc) < 0 || cols <= (col_index as i32 + 3 * dc) {
+                        continue;
+                    }
+                    let ir1: usize = (row_index as i32 + dr) as usize;
+                    let ic1: usize = (col_index as i32 + dc) as usize;
+                    if input[ir1].chars().nth(ic1).unwrap() == 'M'
+                        && input[ir1 + dr as usize]
+                            .chars()
+                            .nth(ic1 + dc as usize)
+                            .unwrap()
+                            == 'A'
+                        && input[ir1 + 2 * dr as usize]
+                            .chars()
+                            .nth(ic1 + 2 * dc as usize)
+                            .unwrap()
+                            == 'S'
+                    {
+                        result += 1;
+                    }
+                }
+            }
+        }
+    }
+    result
+}
+
+#[aoc(day4, part2)]
+pub fn part2(input: &Vec<String>) -> u64 {
+    let mut result: u64 = 0;
+    let rows = input.len();
+    let cols = input[0].len();
+    for (row_index, row) in input.iter().enumerate() {
+        for (col_index, col) in row.char_indices() {
+            if row_index == 0 || row_index == rows - 1 || col_index == 0 || col_index == cols - 1 {
+                continue;
+            }
+            if col != 'A' {
+                continue;
+            }
+            let ul = input[row_index - 1].chars().nth(col_index - 1).unwrap();
+            let dl = input[row_index + 1].chars().nth(col_index - 1).unwrap();
+            let ur = input[row_index - 1].chars().nth(col_index + 1).unwrap();
+            let dr = input[row_index + 1].chars().nth(col_index + 1).unwrap();
+            let ulx = ul as u8;
+            let dlx = dl as u8;
+            let urx = ur as u8;
+            let drx = dr as u8;
+
+            if ulx.abs_diff(drx) == 6 && urx.abs_diff(dlx) == 6 {
+                result += 1;
+            }
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse(SAMPLE)), 18);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse(SAMPLE)), 9);
+    }
+}
