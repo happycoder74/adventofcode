dbc8722904aec16d5caa73a9c26fcdef4d4461ff
Christian <christian@arch>
2025-04-15 19:08:07 +0200

    A new version without regex
    Regex is surprisingly fast in Python compared to loops.

--- python/y2015/d05/aoc_2015_05.py
+++ python/y2015/d05/aoc_2015_05.py
@@ -1,4 +1,3 @@
-import re
 from common import timer, Puzzle
 
 
@@ -9,43 +8,58 @@ class Day05(Puzzle, year=2015, day=5):
 
     @staticmethod
     def check_wovels(line) -> bool:
-        return len(re.findall(r"[aeiou]", line)) >= 3
+        return sum([line.count(c) for c in "aeiou"]) >= 3
 
     @staticmethod
     def check_dbl_letter(line) -> bool:
-        return len(re.findall(r"(.)\1", line)) > 0
+        for i, a in enumerate(line[:-1]):
+            if a == line[i + 1]:
+                return True
+        return False
 
     @staticmethod
     def check_invalid(line) -> bool:
-        pattern = r"(ab|cd|pq|xy)"
-        return len(re.findall(pattern, line)) == 0
+        for p in ["ab", "cd", "pq", "xy"]:
+            if p in line:
+                return False
+        return True
 
     @staticmethod
     def check_pairs(line) -> bool:
-        pattern = r"([a-z][a-z])\w*\1"
-        return len(re.findall(pattern, line)) > 0
+        zipped = list(zip(line, line[1:]))
+        pairs: dict[tuple[str, str], int] = dict()
+        for index, pair in enumerate(zipped):
+            if pair not in pairs:
+                pairs[pair] = index
+                continue
+            if pairs[pair] < index - 1:
+                return True
+        return False
 
     @staticmethod
-    def check_repeat(line) -> bool:
-        pattern = r"(.)\w\1"
-        return len(re.findall(pattern, line)) > 0
+    def check_repeat(line: str) -> bool:
+        return any([a == b for a, b in zip(line, line[2:])])
 
     @staticmethod
     def is_nice(line) -> bool:
-        return (Day05.check_wovels(line)
-                and Day05.check_dbl_letter(line)
-                and Day05.check_invalid(line))
+        return all(
+            [
+                Day05.check_wovels(line),
+                Day05.check_dbl_letter(line),
+                Day05.check_invalid(line),
+            ]
+        )
 
     @staticmethod
     def is_nice_2(line) -> bool:
-        return (Day05.check_pairs(line) and Day05.check_repeat(line))
+        return all([Day05.check_pairs(line), Day05.check_repeat(line)])
 
     @timer(part=1)
     def solve_part_1(self) -> int:
         """Solution for part 1"""
-        return len([1 for line in self.data if Day05.is_nice(line)])
+        return len(list(filter(Day05.is_nice, self.data)))
 
     @timer(part=2)
     def solve_part_2(self) -> int:
         """Solution for part 2"""
-        return len([1 for line in self.data if Day05.is_nice_2(line)])
+        return len(list(filter(Day05.is_nice_2, self.data)))
