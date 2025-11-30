939a25afcd97d0697a79ae4a07f534097b9e830a
Christian <christian@wsl>
2025-11-30 14:34:09 +0100

    Rust 2016 day 6 part 2

--- Rust/2016/day06/src/part2.rs
+++ Rust/2016/day06/src/part2.rs
@@ -1,3 +1,44 @@
+use std::collections::HashMap;
+
 pub fn solve_part(input: &Vec<&str>) -> Result<String, ()> {
-    Ok(String::new())
+    let mut decoded = String::new();
+    for column in 0..input[0].len() {
+        let mut letter_map = HashMap::new();
+        for row in input {
+            let c = row.chars().nth(column).unwrap();
+            *letter_map.entry(c).or_insert(0) += 1;
+        }
+
+        let least_common = letter_map.iter().min_by_key(|(_, v)| *v).unwrap();
+        decoded.push(*least_common.0);
+    }
+
+    Ok(decoded)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve_part;
+
+    #[test]
+    fn test_solve_part() {
+        let test_data = "eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
+        let input: Vec<&str> = test_data.lines().collect();
+        assert_eq!(solve_part(&input).unwrap(), "advent");
+    }
 }
