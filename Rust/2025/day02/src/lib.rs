9daead06746d90f85215eeb29901ddaed8b3c48f
Christian Lundh <christian.lundh@gknaerospace.com>
2025-12-02 15:17:18 +0100

    Rust 2025 day 2 completed

--- Rust/2025/day02/src/lib.rs
+++ Rust/2025/day02/src/lib.rs
@@ -1,34 +1,5 @@
-use std::error::Error;
-use std::fmt::Display;
-
 pub mod part1;
 pub mod part2;
-
-#[derive(Debug)]
-pub struct NotImplementedError {
-    text: String,
-}
-
-impl NotImplementedError {
-    #[allow(clippy::must_use_candidate)]
-    pub fn new(value: &str) -> Self {
-        Self {
-            text: value.to_string(),
-        }
-    }
-}
-
-impl Error for NotImplementedError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        None
-    }
-}
-
-impl Display for NotImplementedError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.text)
-    }
-}
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Instruction {
