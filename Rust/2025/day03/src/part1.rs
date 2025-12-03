792da4b18af3a5be8fea4f254531c6034f910a3a
Christian <christian@wsl>
2025-12-03 20:18:41 +0100

    Rust 2025 day 3. Updated algorithm for part1. Extension of the same method for part 2

--- Rust/2025/day03/src/part1.rs
+++ Rust/2025/day03/src/part1.rs
@@ -1,50 +1,28 @@
 use aoc_utils::NotImplementedError;
 
 #[allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
-pub fn solve_part(input: &str) -> Result<u32, NotImplementedError> {
-    let mut result = 0;
-    let batteries: Vec<Vec<_>> = input
+pub fn solve_part(input: &str) -> Result<usize, NotImplementedError> {
+    let batteries = input
         .lines()
-        .map(str::trim)
-        .map(|s| {
-            s.chars()
-                .map(|c| c.to_digit(10).unwrap())
-                .collect::<Vec<u32>>()
-        })
-        .collect();
-
-    for battery in batteries {
-        let max_index = battery.len() - 1;
-        let (index, voltage) = battery
-            .iter()
-            .enumerate()
-            .rev()
-            .max_by_key(|&(_, val)| val)
-            .unwrap();
-        if index == max_index - 1 {
-            result += voltage * 10 + battery.iter().last().unwrap();
-        } else if index == max_index {
-            result += battery
+        .map(|s| s.trim().as_bytes())
+        .map(|line| {
+            let (i, a) = &line[..line.len() - 1]
                 .iter()
                 .enumerate()
-                .take(index)
+                .rev()
                 .max_by_key(|(_, v)| *v)
-                .unwrap()
-                .1
-                * 10
-                + voltage;
-        } else {
-            result += voltage * 10
-                + battery
-                    .iter()
-                    .enumerate()
-                    .skip(index + 1)
-                    .max_by_key(|(_, v)| *v)
-                    .unwrap()
-                    .1;
-        }
-    }
-    Ok(result)
+                .unwrap();
+            let (_, b) = &line[*i + 1..]
+                .iter()
+                .enumerate()
+                .rev()
+                .max_by_key(|(_, v)| *v)
+                .unwrap();
+            ((*a - b'0') * 10 + *b - b'0') as usize
+        })
+        .sum::<usize>();
+
+    Ok(batteries)
 }
 
 #[cfg(test)]
--- Rust/2025/day03/src/part2.rs
+++ Rust/2025/day03/src/part2.rs
@@ -2,7 +2,34 @@ use aoc_utils::NotImplementedError;
 
 #[allow(clippy::missing_panics_doc, clippy::missing_errors_doc)]
 pub fn solve_part(input: &str) -> Result<u64, NotImplementedError> {
-    Err(NotImplementedError::new("Not implemented"))
+    let mut result = 0usize;
+    let data = input
+        .lines()
+        .map(|line| {
+            line.trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    for bank in data {
+        let mut bank_value = 0usize;
+        let mut bank_line: &[usize] = &bank;
+
+        for p in (0..12).rev() {
+            let (index, max_item) = &bank_line[..bank_line.len() - p]
+                .iter()
+                .enumerate()
+                .rev()
+                .max_by_key(|(_, v)| *v)
+                .unwrap();
+            bank_line = &bank_line[index + 1..];
+            bank_value += *max_item * 10usize.pow(p as u32);
+        }
+        result += bank_value;
+    }
+    Ok(result as u64)
 }
 
 #[cfg(test)]
@@ -15,6 +42,6 @@ mod tests {
 234234234234278
 818181911112111";
 
-        assert_eq!(super::solve_part(input).unwrap(), 0);
+        assert_eq!(super::solve_part(input).unwrap(), 3121910778619);
     }
 }
