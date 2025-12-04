6a7b335c977a01b737c58eddc898adc2ba7a00ec
Christian Lundh <christian@nuc>
2025-12-04 22:58:21 +0100

    Rust 2025 day 4. Modified structure.

--- Rust/2025/day04/src/part1.rs
+++ Rust/2025/day04/src/part1.rs
@@ -1,41 +1,20 @@
 #![allow(unused_variables)]
+use crate::Grid;
 use aoc_utils::NotImplementedError;
-use std::collections::HashSet;
-
-fn count_paperrolls(grid: &HashSet<(i32, i32)>) -> usize {
-    let result = grid
-        .iter()
-        .filter(|(r, c)| {
-            let mut n_nb = 0;
-            for dr in -1..=1 {
-                for dc in -1..=1 {
-                    if !((dc == 0) && (dr == 0)) {
-                        let nb = (r + dr, c + dc);
-                        if grid.contains(&nb) {
-                            n_nb += 1;
-                        }
-                    }
-                }
-            }
-            n_nb < 4
-        })
-        .count();
-    result
-}
 
 #[allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 pub fn solve_part(input: &str) -> Result<usize, NotImplementedError> {
-    let mut grid = HashSet::new();
+    let mut grid = Grid::new();
     let lines = input.lines().map(str::trim).enumerate().collect::<Vec<_>>();
     for (irow, row) in &lines {
         row.chars().enumerate().for_each(|(icol, col)| {
             if col == '@' {
-                grid.insert((*irow as i32, icol as i32));
+                grid.grid.insert((*irow as i32, icol as i32));
             }
         });
     }
 
-    Ok(count_paperrolls(&grid))
+    Ok(grid.accessible())
 }
 
 #[cfg(test)]
